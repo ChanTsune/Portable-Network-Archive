45b2c44ed846eeae418cb873120ad77c522d7d6a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-19 00:01:19 +0900

    Add cli if

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,7 @@ module pna
 
 go 1.15
 
-require github.com/DataDog/zstd v1.4.8
+require (
+	github.com/DataDog/zstd v1.4.8
+	github.com/urfave/cli v1.22.5
+)
--- go/main.go
+++ go/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"pna/pna"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	if err := pna.ArchiveAll("./pna", "./sample.pna"); err != nil {
-		fmt.Println(err.Error())
-		return
+	app := cli.NewApp()
+	app.Name = "Potable-Network-Archive"
+	app.Version = "0.0.0"
+
+	app.Action = func(context *cli.Context) error {
+		return cli.ShowAppHelp(context)
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		println(err.Error())
+	}
+}
+
+func extractProcess(context *cli.Context) error {
 	if err := pna.ExtractAll("./ext", "./sample.pna"); err != nil {
-		fmt.Println(err.Error())
-		return
+		return err
 	}
+	return nil
+}
+
+func archiveProcess(context *cli.Context) error {
+	if err := pna.ArchiveAll("./pna", "./sample.pna"); err != nil {
+		return err
+	}
+	return nil
 }
