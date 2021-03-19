748aa0b7a20ab7aca214dbaaf02d3c881298d3cb
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-20 00:08:48 +0900

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"os"
 	"pna/pna"
 
@@ -10,27 +12,52 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "Potable-Network-Archive"
+	app.Usage = ""
 	app.Version = "0.0.0"
 
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "create, c",
+			Usage: "create archive",
+		},
+		cli.BoolFlag{
+			Name:  "extract, x",
+			Usage: "extarct archive",
+		},
+	}
+
 	app.Action = func(context *cli.Context) error {
+		if context.Bool("c") {
+			return archiveProcess(context)
+		} else if context.Bool("x") {
+			return extractProcess(context)
+		}
 		return cli.ShowAppHelp(context)
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		println(err.Error())
+		log.Fatalln(err)
 	}
 }
 
 func extractProcess(context *cli.Context) error {
-	if err := pna.ExtractAll("./ext", "./sample.pna"); err != nil {
+	archiveName := context.Args().First()
+	if len(archiveName) == 0 {
+		return errors.New("no files or directories specified")
+	}
+	if err := pna.ExtractAll("./ext", archiveName); err != nil {
 		return err
 	}
 	return nil
 }
 
 func archiveProcess(context *cli.Context) error {
-	if err := pna.ArchiveAll("./pna", "./sample.pna"); err != nil {
+	archiveName := context.Args().First()
+	if len(archiveName) == 0 {
+		return errors.New("no files or directories specified")
+	}
+	if err := pna.ArchiveAll("./pna", archiveName); err != nil {
 		return err
 	}
 	return nil
