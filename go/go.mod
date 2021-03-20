9f95e6e522b02b4858c15101ef8c29040e75a6c7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-21 00:40:09 +0900

    crypto

--- go/go.mod
+++ go/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/DataDog/zstd v1.4.8
 	github.com/dgryski/go-camellia v0.0.0-20191119043421-69a8a13fb23d
 	github.com/urfave/cli v1.22.5
+	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670
 )
--- go/pna/utils/utils.go
+++ go/pna/utils/utils.go
@@ -1,9 +1,14 @@
 package utils
 
 import (
+	"crypto/rand"
+	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
+	"math/big"
 	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 func Uint32ToBytes(n uint32) []byte {
@@ -25,4 +30,26 @@ func Bytes2str(bytes ...byte) string {
 		strs = append(strs, fmt.Sprintf("%02x", b))
 	}
 	return strings.Join(strs, " ")
+}
+
+func randUint8() uint8 {
+	i, err := rand.Int(rand.Reader, big.NewInt(128))
+	if err != nil {
+		panic(err.Error())
+	}
+	return uint8(i.Int64())
+}
+
+func RandBytes(n int) []byte {
+	// TODO: Uint8 to Uint64
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = randUint8()
+	}
+	return b
+}
+
+func StretchPassword(password string) ([]byte, []byte) {
+	salt := RandBytes(16)
+	return salt, pbkdf2.Key([]byte(password), salt, 10000, 32, sha1.New)
 }
