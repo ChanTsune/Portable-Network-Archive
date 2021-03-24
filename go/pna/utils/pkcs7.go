bddbaa348f8e0f7fd836bbab1437f4c50e258b21
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-24 18:03:15 +0900

    Split file

---
+++ go/pna/utils/pkcs7.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+)
+
+// pkcs7unpad remove pkcs7 padding
+func pkcs7unpad(data []byte, blockSize int) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, errors.New("pkcs7: Data is empty")
+	}
+	if length%blockSize != 0 {
+		return nil, errors.New("pkcs7: Data is not block-aligned")
+	}
+	padLen := int(data[length-1])
+	ref := bytes.Repeat([]byte{byte(padLen)}, padLen)
+	if padLen > blockSize || padLen == 0 || !bytes.HasSuffix(data, ref) {
+		return nil, errors.New("pkcs7: Invalid padding")
+	}
+	return data[:length-padLen], nil
+}
+
+// pkcs7pad add pkcs7 padding
+func pkcs7pad(data []byte, blockSize int) ([]byte, error) {
+	if blockSize < 0 || blockSize > 256 {
+		return nil, fmt.Errorf("pkcs7: Invalid block size %d", blockSize)
+	} else {
+		padLen := blockSize - len(data)%blockSize
+		padding := bytes.Repeat([]byte{byte(padLen)}, padLen)
+		return append(data, padding...), nil
+	}
+}
