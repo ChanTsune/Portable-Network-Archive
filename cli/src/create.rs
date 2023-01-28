9f68576523aee4dc52e4464e5342cb1b77999219
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-01-28 13:36:53 +0900

    Update cli

---
+++ cli/src/create.rs
@@ -0,0 +1,32 @@
+use crate::Options;
+use std::{fs::File, io, path::Path};
+
+pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
+    archive: A,
+    files: &[F],
+    options: Options,
+) -> io::Result<()> {
+    let archive = archive.as_ref();
+    if archive.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::AlreadyExists,
+            format!("{} is alrady exists", archive.display()),
+        ));
+    }
+    if !options.quiet {
+        println!("Create an archive: {}", archive.display());
+    }
+    File::create(archive)?;
+
+    for file in files {
+        let file = file.as_ref();
+        if !options.quiet && options.verbose {
+            println!("Adding: {}", file.display());
+        }
+    }
+
+    if !options.quiet {
+        println!("Successfully created an archive");
+    }
+    Ok(())
+}
