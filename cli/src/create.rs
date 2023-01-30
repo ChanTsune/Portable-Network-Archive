82ff65a75015925cd15589b168a8212d19c806bd
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-01-30 19:02:46 +0900

    fix non-existent path in cli causes archive creation to fail.

--- cli/src/create.rs
+++ cli/src/create.rs
@@ -1,6 +1,7 @@
 use crate::Options;
 use libpna::{ArchiveWriter, Encoder};
 use std::{
+    env,
     fs::{self, File},
     io::{self, Write},
     path::Path,
@@ -21,17 +22,23 @@ pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
     if !options.quiet {
         println!("Create an archive: {}", archive.display());
     }
-    let file = File::create(archive)?;
+    let temp_dir = env::temp_dir();
+    fs::create_dir_all(&temp_dir)?;
+    let temp_file_path = temp_dir.join(archive.file_name().unwrap());
+
+    let file = File::create(&temp_file_path)?;
 
     let encoder = Encoder::new();
     let mut writer = encoder.write_header(file)?;
 
     for file in files {
         let file = file.as_ref();
-        write_internal(&mut writer, file, archive, &options)?;
+        write_internal(&mut writer, file, &options)?;
     }
 
     writer.finalize()?;
+
+    fs::rename(temp_file_path, archive)?;
 
     if !options.quiet {
         println!("Successfully created an archive");
@@ -42,19 +49,15 @@ pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
 fn write_internal<W: Write>(
     writer: &mut ArchiveWriter<W>,
     path: &Path,
-    ignore: &Path,
     options: &Options,
 ) -> io::Result<()> {
-    if path.canonicalize()? == ignore.canonicalize()? {
-        return Ok(());
-    }
     if !options.quiet && options.verbose {
         println!("Adding: {}", path.display());
     }
     if path.is_dir() {
         if options.recursive {
             for i in fs::read_dir(path)? {
-                write_internal(writer, &i?.path(), ignore, options)?;
+                write_internal(writer, &i?.path(), options)?;
             }
         }
     } else if path.is_file() {
