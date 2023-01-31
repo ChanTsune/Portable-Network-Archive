8d898847c9afb16d32bdb8e42fa7572a6775b967
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-01-31 09:42:54 +0900

    fix missing commit

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,7 @@
 pub(crate) mod archive;
 pub(crate) mod chunk;
 
-pub use archive::{ArchiveReader, ArchiveWriter, Decoder, Encoder, PNA_HEADER};
+pub use archive::{
+    ArchiveReader, ArchiveWriter, Compression, Decoder, Encoder, Options, PNA_HEADER,
+};
 pub use chunk::*;
