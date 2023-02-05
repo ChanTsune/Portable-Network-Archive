dbc79499ecf97d80733475195008840fdf03bf6d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-02-05 14:16:49 +0900

    error handling

--- lib/src/archive/read.rs
+++ lib/src/archive/read.rs
@@ -38,7 +38,6 @@ impl<R: Read> ArchiveReader<R> {
         let mut all_data: Vec<u8> = vec![];
         let mut info = None;
         let mut phsf = None;
-        let mut s;
         loop {
             let (chunk_type, mut raw_data) = self.r.read_chunk()?;
             match chunk_type {
@@ -47,8 +46,10 @@ impl<R: Read> ArchiveReader<R> {
                     info = Some(from_chunk_data_fhed(&raw_data)?);
                 }
                 chunk::PHSF => {
-                    s = String::from_utf8(raw_data).unwrap();
-                    phsf = Some(verify_password(&s, password.unwrap()));
+                    phsf = Some(
+                        String::from_utf8(raw_data)
+                            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
+                    );
                 }
                 chunk::AEND => return Ok(None),
                 chunk::FDAT => all_data.append(&mut raw_data),
@@ -73,7 +74,22 @@ impl<R: Read> ArchiveReader<R> {
         let all_data = match info.encryption {
             Encryption::No => all_data,
             Encryption::Aes => {
-                decrypt_aes256_cbc(phsf.unwrap().hash.unwrap().as_bytes(), &all_data)?
+                let s = phsf.ok_or_else(|| {
+                    io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        format!("Item is encrypted, but `PHSF` chunk not found"),
+                    )
+                })?;
+                let phsf = verify_password(
+                    &s,
+                    password.ok_or_else(|| {
+                        io::Error::new(
+                            io::ErrorKind::InvalidInput,
+                            String::from("Item is encrypted, but password was not provided"),
+                        )
+                    })?,
+                );
+                decrypt_aes256_cbc(phsf.hash.unwrap().as_bytes(), &all_data)?
             }
             Encryption::Camellia => todo!(),
         };
