1b00feb445ff30f1e7d1f8ab65aa1fa90a215a43
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-02-10 17:33:30 +0900

    Create archive_empty.rs

---
+++ lib/benches/archive_empty.rs
@@ -0,0 +1,34 @@
+#![feature(test)]
+extern crate test;
+
+use libpna::{Decoder, Encoder};
+use std::io::{self, Cursor};
+use test::Bencher;
+
+#[bench]
+fn write_empty_archive(b: &mut Bencher) {
+    b.iter(|| {
+        let mut vec = Vec::with_capacity(1000);
+        let encoder = Encoder::default();
+        let mut writer = encoder.write_header(&mut vec).unwrap();
+        writer.finalize().unwrap();
+    })
+}
+
+#[bench]
+fn read_empty_archive(b: &mut Bencher) {
+    let mut vec = Vec::with_capacity(1000);
+    {
+        let encoder = Encoder::default();
+        let mut writer = encoder.write_header(&mut vec).unwrap();
+        writer.finalize().unwrap();
+    }
+
+    b.iter(|| {
+        let decoder = Decoder::default();
+        let mut reader = decoder.read_header(Cursor::new(vec.as_slice())).unwrap();
+        while let Some(mut item) = reader.read(None).unwrap() {
+            io::read_to_string(item).unwrap();
+        }
+    })
+}
