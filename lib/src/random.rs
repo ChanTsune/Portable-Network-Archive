75283fb3dcc79a410ad74e6a4090a37b3f9bb41a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-02-25 23:19:40 +0900

    add random::random_vec

--- lib/src/random.rs
+++ lib/src/random.rs
@@ -2,12 +2,18 @@ use password_hash::SaltString;
 use rand::{rngs::OsRng, RngCore};
 use std::io;
 
-pub fn random_bytes(dist: &mut [u8]) -> io::Result<()> {
+pub(crate) fn random_bytes(dist: &mut [u8]) -> io::Result<()> {
     let mut rand = OsRng::default();
     rand.try_fill_bytes(dist)
         .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
 }
 
-pub fn salt_string() -> SaltString {
+pub(crate) fn random_vec(size: usize) -> io::Result<Vec<u8>> {
+    let mut v = vec![0; size];
+    random_bytes(&mut v)?;
+    Ok(v)
+}
+
+pub(crate) fn salt_string() -> SaltString {
     SaltString::generate(OsRng::default())
 }
