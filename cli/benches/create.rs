e38565c4ddb985b44710ce522711eeaf11c4cdcc
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-08 16:32:38 +0900

    Create benches/create.rs

---
+++ cli/benches/create.rs
@@ -0,0 +1,71 @@
+
+#![feature(test)]
+extern crate test;
+
+use clap::Parser;
+use portable_network_archive::command;
+use test::Bencher;
+
+#[bench]
+fn store(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(command::Args::parse_from([
+            "pna",
+            "-c",
+            "../out/store.pna",
+            "--store",
+            "--overwrite",
+            "-r",
+            "../resources/test/raw/",
+        ]))
+        .unwrap()
+    })
+}
+
+#[bench]
+fn zstd(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(command::Args::parse_from([
+            "pna",
+            "-c",
+            "../out/zstd.pna",
+            "--store",
+            "--overwrite",
+            "-r",
+            "../resources/test/raw/",
+        ]))
+        .unwrap()
+    })
+}
+
+#[bench]
+fn deflate(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(command::Args::parse_from([
+            "pna",
+            "-c",
+            "../out/deflate.pna",
+            "--store",
+            "--overwrite",
+            "-r",
+            "../resources/test/raw/",
+        ]))
+        .unwrap()
+    })
+}
+
+#[bench]
+fn lzma(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(command::Args::parse_from([
+            "pna",
+            "-c",
+            "../out/lzma.pna",
+            "--store",
+            "--overwrite",
+            "-r",
+            "../resources/test/raw/",
+        ]))
+        .unwrap()
+    })
+}
