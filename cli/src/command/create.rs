062fc53756b16de4812a7e9672ceb0d9ea612741
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-09 17:56:35 +0900

    Collect items before write

--- cli/src/command/create.rs
+++ cli/src/command/create.rs
@@ -1,5 +1,6 @@
 use super::{CipherMode, Options};
 use libpna::{ArchiveWriter, Encoder};
+use std::path::PathBuf;
 use std::{
     fs::{self, File},
     io::{self, Write},
@@ -21,6 +22,11 @@ pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
     if !options.quiet {
         println!("Create an archive: {}", archive.display());
     }
+    let mut target_items = vec![];
+    for p in files {
+        collect_items(&mut target_items, p.as_ref(), &options)?;
+    }
+
     if let Some(parent) = archive.parent() {
         fs::create_dir_all(parent)?;
     }
@@ -29,9 +35,9 @@ pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
     let encoder = Encoder::new();
     let mut writer = encoder.write_header(file)?;
 
-    for file in files {
+    for file in target_items {
         let file = file.as_ref();
-        write_internal(&mut writer, file, archive, &options)?;
+        write_internal(&mut writer, file, &options)?;
     }
 
     writer.finalize()?;
@@ -42,25 +48,28 @@ pub(crate) fn create_archive<A: AsRef<Path>, F: AsRef<Path>>(
     Ok(())
 }
 
-fn write_internal<W: Write>(
-    writer: &mut ArchiveWriter<W>,
-    path: &Path,
-    ignore: &Path,
-    options: &Options,
-) -> io::Result<()> {
-    if path.canonicalize()? == ignore.canonicalize()? {
-        return Ok(());
-    }
-    if !options.quiet && options.verbose {
-        println!("Adding: {}", path.display());
-    }
+fn collect_items(result: &mut Vec<PathBuf>, path: &Path, options: &Options) -> io::Result<()> {
     if path.is_dir() {
         if options.recursive {
-            for i in fs::read_dir(path)? {
-                write_internal(writer, &i?.path(), ignore, options)?;
+            for p in fs::read_dir(path)? {
+                collect_items(result, &p?.path(), options)?;
             }
         }
     } else if path.is_file() {
+        result.push(path.to_path_buf());
+    }
+    Ok(())
+}
+
+fn write_internal<W: Write>(
+    writer: &mut ArchiveWriter<W>,
+    path: &Path,
+    options: &Options,
+) -> io::Result<()> {
+    if !options.quiet && options.verbose {
+        println!("Adding: {}", path.display());
+    }
+    if path.is_file() {
         let mut item_option = libpna::Options::default();
         if options.store {
             item_option = item_option.compression(libpna::Compression::No);
