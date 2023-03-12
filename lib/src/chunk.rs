542be54e919779d6d55cfa1c8d0274350c4e7350
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-12 18:42:26 +0900

    Add chunk_to_bytes

--- lib/src/chunk.rs
+++ lib/src/chunk.rs
@@ -9,7 +9,7 @@ use std::ops::Deref;
 pub use types::*;
 pub(crate) use write::ChunkWriter;
 
-trait Chunk {
+pub(crate) trait Chunk {
     fn length(&self) -> u32 {
         self.data().len() as u32
     }
@@ -66,13 +66,34 @@ pub(crate) fn create_chunk_data_fhed(
     data.into_boxed_slice()
 }
 
+pub(crate) fn chunk_to_bytes(chunk: impl Chunk) -> Vec<u8> {
+    let mut vec = Vec::with_capacity(12usize + chunk.length() as usize);
+    vec.extend_from_slice(&chunk.length().to_be_bytes());
+    vec.extend_from_slice(&chunk.ty().0);
+    vec.extend_from_slice(chunk.data());
+    vec.extend_from_slice(&chunk.crc().to_be_bytes());
+    vec
+}
+
 #[cfg(test)]
 mod tests {
     use crate::create_chunk_data_ahed;
+    use super::*;
 
     #[test]
     fn ahed() {
         assert_eq!([0u8, 0, 0, 0, 0, 0, 0, 0], create_chunk_data_ahed(0, 0, 0));
         assert_eq!([1u8, 2, 0, 0, 0, 0, 0, 3], create_chunk_data_ahed(1, 2, 3));
+    }
+
+    #[test]
+    fn to_bytes() {
+        assert_eq!(
+            chunk_to_bytes((FDAT, "text data".as_bytes())),
+            [
+                0, 0, 0, 9, 70, 68, 65, 84, 116, 101, 120, 116, 32, 100, 97, 116, 97, 177, 70, 138,
+                128
+            ]
+        )
     }
 }
