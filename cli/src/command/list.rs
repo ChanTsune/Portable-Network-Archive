6a408c4079f4c1f9608b527299f1874fc4f6501a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-16 18:01:04 +0900

    cli Beta Support list item details

--- cli/src/command/list.rs
+++ cli/src/command/list.rs
@@ -1,11 +1,14 @@
+mod table;
+
 use crate::cli::{ListArgs, Verbosity};
+use crate::command::list::table::{Cell, Table, TableRow};
+use ansi_term::{Colour, Style};
 use glob::Pattern;
-use libpna::{Decoder, ReadEntry};
+use libpna::{Decoder, Encryption, EntryHeader, ReadEntry};
 use std::fs::File;
 use std::io;
-use std::path::PathBuf;
 
-pub(crate) fn list_archive(args: ListArgs, verbosity: Verbosity) -> io::Result<()> {
+pub(crate) fn list_archive(args: ListArgs, _: Verbosity) -> io::Result<()> {
     let globs = args
         .file
         .files
@@ -15,17 +18,63 @@ pub(crate) fn list_archive(args: ListArgs, verbosity: Verbosity) -> io::Result<(
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
     let file = File::open(args.file.archive)?;
 
+    let mut entries = vec![];
     let decoder = Decoder::new();
     let mut reader = decoder.read_header(file)?;
     while let Some(item) = reader.read()? {
-        let path = PathBuf::from(item.header().path().as_str());
-        if !globs.is_empty() && !globs.iter().any(|glob| glob.matches_path(&path)) {
-            if verbosity == Verbosity::Verbose {
-                println!("Skip: {}", item.header().path())
-            }
-            continue;
-        }
-        println!("{}", path.display())
+        entries.push(item.header().clone());
+    }
+
+    if entries.is_empty() {
+        return Ok(());
+    }
+
+    let entries = if globs.is_empty() {
+        entries
+    } else {
+        entries
+            .into_iter()
+            .filter(|h| globs.iter().any(|glob| glob.matches(h.path().as_ref())))
+            .collect()
+    };
+    if args.long {
+        detail_list_entries(&entries);
+    } else {
+        simple_list_entries(&entries);
     }
     Ok(())
+}
+
+fn simple_list_entries(entries: &[EntryHeader]) {
+    for entry in entries {
+        println!("{}", entry.path())
+    }
+}
+
+fn detail_list_entries(entries: &[EntryHeader]) {
+    let style_header_line = Style::new().underline();
+    let style_encryption_column = Style::new().fg(Colour::Purple);
+    let style_compression_column = Style::new().fg(Colour::Blue);
+    let style_entry = Style::new();
+    let mut table = Table::new(style_header_line);
+    for entry in entries {
+        table.push(TableRow::new(vec![
+            Cell::new(
+                style_encryption_column,
+                match entry.encryption() {
+                    Encryption::No => "-".to_string(),
+                    _ => format!("{:?}({:?})", entry.encryption(), entry.cipher_mode())
+                        .to_ascii_lowercase(),
+                },
+            ),
+            Cell::new(
+                style_compression_column,
+                format!("{:?}", entry.compression()).to_ascii_lowercase(),
+            ),
+            Cell::new(style_entry, entry.path()),
+        ]));
+    }
+    for row in table.into_render_rows() {
+        println!("{}", row)
+    }
 }
