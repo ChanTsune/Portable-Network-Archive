064ee8d09550906531a53368a0a6ecf9beb249da
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-19 20:00:43 +0900

    Bench unuse macro

--- lib/benches/create_extract/main.rs
+++ lib/benches/create_extract/main.rs
@@ -1,61 +1,62 @@
 #![feature(test)]
+extern crate test;
+
+use libpna::{
+    ArchiveReader, Encoder, EntryBuilder, ReadEntry, ReadOptionBuilder, WriteOptionBuilder,
+};
+use std::io::{Cursor, Read, Write};
+use test::Bencher;
+
 mod archive;
 mod empty;
 
-#[macro_export]
-macro_rules! bench_write_archive {
-    ($name:ident, $options:expr) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
-            b.iter(|| {
-                let mut vec = Vec::with_capacity(10000);
-                let encoder = Encoder::default();
-                let mut writer = encoder.write_header(&mut vec).unwrap();
-                writer
-                    .start_file_with_options(
-                        "bench".into(),
-                        $options.password(Some("password")).build(),
-                    )
-                    .unwrap();
-                writer.write_all(&vec![24; 1111]).unwrap();
-                writer.end_file().unwrap();
-                writer.finalize().unwrap();
+fn bench_write_archive(b: &mut Bencher, mut options: WriteOptionBuilder) {
+    b.iter(|| {
+        let mut vec = Vec::with_capacity(10000);
+        let encoder = Encoder::default();
+        let mut writer = encoder.write_header(&mut vec).unwrap();
+        writer
+            .add_entry({
+                let mut builder = EntryBuilder::new_file(
+                    "bench".into(),
+                    options.password(Some("password")).build(),
+                )
+                .unwrap();
+                builder.write_all(&vec![24; 1111]).unwrap();
+                builder.build().unwrap()
             })
-        }
-    };
+            .unwrap();
+        writer.finalize().unwrap();
+    })
 }
 
-#[macro_export]
-macro_rules! bench_read_archive {
-    ($name:ident, $options:expr) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
-            let mut vec = Vec::with_capacity(10000);
-            {
-                let encoder = Encoder::default();
-                let mut writer = encoder.write_header(&mut vec).unwrap();
-                writer
-                    .start_file_with_options(
-                        "bench".into(),
-                        $options.password(Some("password")).build(),
-                    )
-                    .unwrap();
-                writer.write_all(&vec![24; 1111]).unwrap();
-                writer.end_file().unwrap();
-                writer.finalize().unwrap();
-            }
-
-            b.iter(|| {
-                let decoder = Decoder::default();
-                let mut reader = decoder.read_header(Cursor::new(vec.as_slice())).unwrap();
-                while let Some(item) = reader.read().unwrap() {
-                    let mut buf = Vec::with_capacity(1000);
-                    item.into_reader(ReadOptionBuilder::new().password("password").build())
-                        .unwrap()
-                        .read_to_end(&mut buf)
-                        .unwrap();
-                }
+fn bench_read_archive(b: &mut Bencher, mut options: WriteOptionBuilder) {
+    let mut vec = Vec::with_capacity(10000);
+    {
+        let encoder = Encoder::default();
+        let mut writer = encoder.write_header(&mut vec).unwrap();
+        writer
+            .add_entry({
+                let mut builder = EntryBuilder::new_file(
+                    "bench".into(),
+                    options.password(Some("password")).build(),
+                )
+                .unwrap();
+                builder.write_all(&vec![24; 1111]).unwrap();
+                builder.build().unwrap()
             })
+            .unwrap();
+        writer.finalize().unwrap();
+    }
+
+    b.iter(|| {
+        let mut reader = ArchiveReader::read_header(Cursor::new(vec.as_slice())).unwrap();
+        while let Some(item) = reader.read().unwrap() {
+            let mut buf = Vec::with_capacity(1000);
+            item.into_reader(ReadOptionBuilder::new().password("password").build())
+                .unwrap()
+                .read_to_end(&mut buf)
+                .unwrap();
         }
-    };
+    })
 }
