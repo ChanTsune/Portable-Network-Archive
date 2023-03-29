4f4742f4d51b16b40b1c996e81499c3af88d3c2d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-03-29 20:48:16 +0900

    use `ChunkEntry` instead of `BytesEntry`

--- lib/src/archive/entry/builder.rs
+++ lib/src/archive/entry/builder.rs
@@ -1,9 +1,9 @@
 use crate::{
     archive::entry::{
-        write::writer_and_hash, BytesEntry, DataKind, Entry, EntryHeader, EntryName, Permission,
+        write::writer_and_hash, ChunkEntry, DataKind, Entry, EntryHeader, EntryName, Permission,
         WriteOption,
     },
-    chunk::{ChunkType, ChunkWriter},
+    chunk::{ChunkType, RawChunk},
     cipher::CipherWriter,
     compress::CompressionWriter,
     io::TryIntoInner,
@@ -101,36 +101,38 @@ impl EntryBuilder {
     ///
     /// A Result containing the new [Entry], or an I/O error if the build fails.
     pub fn build(self) -> io::Result<impl Entry> {
+        Ok(ChunkEntry(self.build_as_chunks()?))
+    }
+
+    fn build_as_chunks(self) -> io::Result<Vec<RawChunk>> {
         let data = self.data.try_into_inner()?.try_into_inner()?;
 
-        let mut chunk_writer = ChunkWriter::from(Vec::with_capacity(data.len() + 128));
-        chunk_writer.write_chunk((ChunkType::FHED, self.header.to_bytes()))?;
+        let mut chunks = vec![];
+        chunks.push(RawChunk::from_data(ChunkType::FHED, self.header.to_bytes()));
+
         if let Some(since_unix_epoch) = self.created {
-            chunk_writer.write_chunk((
+            chunks.push(RawChunk::from_data(
                 ChunkType::cTIM,
-                since_unix_epoch.as_secs().to_be_bytes().as_slice(),
-            ))?;
+                since_unix_epoch.as_secs().to_be_bytes().to_vec(),
+            ));
         }
         if let Some(since_unix_epoch) = self.last_modified {
-            chunk_writer.write_chunk((
+            chunks.push(RawChunk::from_data(
                 ChunkType::mTIM,
-                since_unix_epoch.as_secs().to_be_bytes().as_slice(),
-            ))?;
+                since_unix_epoch.as_secs().to_be_bytes().to_vec(),
+            ));
         }
         if let Some(permission) = self.permission {
-            chunk_writer.write_chunk((ChunkType::fPRM, permission.to_bytes()))?;
+            chunks.push(RawChunk::from_data(ChunkType::fPRM, permission.to_bytes()));
         }
         if let Some(phsf) = self.phsf {
-            chunk_writer.write_chunk((ChunkType::PHSF, phsf.as_bytes()))?;
+            chunks.push(RawChunk::from_data(ChunkType::PHSF, phsf.into_bytes()));
         }
-
         for data_chunk in data.chunks(u32::MAX as usize) {
-            chunk_writer.write_chunk((ChunkType::FDAT, data_chunk))?;
+            chunks.push(RawChunk::from_data(ChunkType::FDAT, data_chunk.to_vec()));
         }
-
-        chunk_writer.write_chunk((ChunkType::FEND, [].as_slice()))?;
-
-        Ok(BytesEntry(chunk_writer.into_inner()))
+        chunks.push(RawChunk::from_data(ChunkType::FEND, Vec::new()));
+        Ok(chunks)
     }
 }
 
