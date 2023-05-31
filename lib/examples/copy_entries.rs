f7304540d7c90af6a4b7e7e46204447382eada91
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-05-31 23:17:32 +0900

    Create example copy_entries.rs

---
+++ lib/examples/copy_entries.rs
@@ -0,0 +1,21 @@
+use libpna::{ArchiveReader, ArchiveWriter};
+use std::io::{self, Read, Write};
+
+/// Simply copy the entries to another archive
+fn copy_entries<R: Read, W: Write>(r: R, w: W) -> io::Result<()> {
+    let mut writer = ArchiveWriter::write_header(w)?;
+    let mut reader = ArchiveReader::read_header(r)?;
+    for entry in reader.entries() {
+        writer.add_entry(entry?)?;
+    }
+    writer.finalize()?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let mut dist = vec![];
+    copy_entries(
+        include_bytes!("../../resources/test/deflate.pna").as_slice(),
+        &mut dist,
+    )
+}
