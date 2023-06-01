24fd342a9190c90787c2a227ace4749d226e630d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-06-02 00:31:52 +0900

    Create example change_compression_method.rs

---
+++ lib/examples/change_compression_method.rs
@@ -0,0 +1,37 @@
+use libpna::{
+    ArchiveReader, ArchiveWriter, Compression, EntryBuilder, ReadEntry, ReadOptionBuilder,
+    WriteOptionBuilder,
+};
+use std::io::{self, Read, Write};
+
+/// Change the entries compression method
+fn change_compression_method<R: Read, W: Write>(
+    r: R,
+    w: W,
+    compression: Compression,
+) -> io::Result<()> {
+    let mut writer = ArchiveWriter::write_header(w)?;
+    let mut reader = ArchiveReader::read_header(r)?;
+    for entry in reader.entries() {
+        let entry = entry?;
+        let header = entry.header();
+        let mut builder = EntryBuilder::new_file(
+            header.path().clone(),
+            WriteOptionBuilder::new().compression(compression).build(),
+        )?;
+        let mut reader = entry.into_reader(ReadOptionBuilder::new().build())?;
+        io::copy(&mut reader, &mut builder)?;
+        writer.add_entry(builder.build()?)?;
+    }
+    writer.finalize()?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let mut dist = vec![];
+    change_compression_method(
+        include_bytes!("../../resources/test/deflate.pna").as_slice(),
+        &mut dist,
+        Compression::Deflate,
+    )
+}
