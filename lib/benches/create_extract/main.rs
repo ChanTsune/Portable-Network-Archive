07f4f8f90a86a6b32f287e27f4a819e506b9e3b3
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-06-28 13:39:41 +0900

    :white_check_mark: use new API in bench

--- lib/benches/create_extract/main.rs
+++ lib/benches/create_extract/main.rs
@@ -2,7 +2,7 @@
 extern crate test;
 
 use libpna::{
-    ArchiveReader, ArchiveWriter, EntryBuilder, ReadEntry, ReadOptionBuilder, WriteOptionBuilder,
+    ArchiveReader, ArchiveWriter, EntryBuilder, ReadEntry, ReadOption, WriteOptionBuilder,
 };
 use std::io::{Cursor, Read, Write};
 use test::Bencher;
@@ -17,7 +17,7 @@ fn bench_write_archive(b: &mut Bencher, mut options: WriteOptionBuilder) {
         writer
             .add_entry({
                 let mut builder = EntryBuilder::new_file(
-                    "bench".into(),
+                    "bench".try_into().unwrap(),
                     options.password(Some("password")).build(),
                 )
                 .unwrap();
@@ -33,9 +33,11 @@ fn bench_read_archive(b: &mut Bencher, mut options: WriteOptionBuilder) {
     let mut writer = ArchiveWriter::write_header(Vec::with_capacity(10000)).unwrap();
     writer
         .add_entry({
-            let mut builder =
-                EntryBuilder::new_file("bench".into(), options.password(Some("password")).build())
-                    .unwrap();
+            let mut builder = EntryBuilder::new_file(
+                "bench".try_into().unwrap(),
+                options.password(Some("password")).build(),
+            )
+            .unwrap();
             builder.write_all(&vec![24; 1111]).unwrap();
             builder.build().unwrap()
         })
@@ -44,9 +46,10 @@ fn bench_read_archive(b: &mut Bencher, mut options: WriteOptionBuilder) {
 
     b.iter(|| {
         let mut reader = ArchiveReader::read_header(Cursor::new(vec.as_slice())).unwrap();
-        while let Some(item) = reader.read().unwrap() {
+        for item in reader.entries() {
             let mut buf = Vec::with_capacity(1000);
-            item.into_reader(ReadOptionBuilder::new().password("password").build())
+            item.unwrap()
+                .into_reader(ReadOption::builder().password("password").build())
                 .unwrap()
                 .read_to_end(&mut buf)
                 .unwrap();
