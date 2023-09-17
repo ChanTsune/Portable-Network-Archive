38f990e890b40cb2cb1a379681454fa27b9948dc
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-09-17 17:15:33 +0900

    :recycle: keep `ArchiveHeader` in `ArchiveWriter`

--- lib/src/archive/write.rs
+++ lib/src/archive/write.rs
@@ -7,7 +7,7 @@ use std::io::{self, Write};
 /// A writer for Portable-Network-Archive.
 pub struct ArchiveWriter<W: Write> {
     w: W,
-    archive_number: u32,
+    header: ArchiveHeader,
 }
 
 impl<W: Write> ArchiveWriter<W> {
@@ -33,16 +33,12 @@ impl<W: Write> ArchiveWriter<W> {
 
     fn write_header_with_archive_number(mut write: W, archive_number: u32) -> io::Result<Self> {
         write.write_all(PNA_HEADER)?;
+        let header = ArchiveHeader::new(0, 0, archive_number);
         let mut chunk_writer = ChunkWriter::from(write);
-        chunk_writer.write_chunk((
-            ChunkType::AHED,
-            ArchiveHeader::new(0, 0, archive_number)
-                .to_bytes()
-                .as_slice(),
-        ))?;
+        chunk_writer.write_chunk((ChunkType::AHED, header.to_bytes().as_slice()))?;
         Ok(Self {
             w: chunk_writer.into_inner(),
-            archive_number,
+            header,
         })
     }
 
@@ -142,7 +138,7 @@ impl<W: Write> ArchiveWriter<W> {
     }
 
     pub fn split_to_next_archive<OW: Write>(mut self, writer: OW) -> io::Result<ArchiveWriter<OW>> {
-        let next_archive_number = self.archive_number + 1;
+        let next_archive_number = self.header.archive_number + 1;
         self.add_next_archive_marker()?;
         self.finalize()?;
         ArchiveWriter::write_header_with_archive_number(writer, next_archive_number)
