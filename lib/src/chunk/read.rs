d8408cfab665ec4c4edacaa834e8f7dd1f6383b0
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-09-18 14:08:53 +0900

    :zap: Add `ChunkReader<Read + Seek>::skip_chunk`

--- lib/src/chunk/read.rs
+++ lib/src/chunk/read.rs
@@ -1,5 +1,5 @@
 use crate::chunk::{crc::Crc32, ChunkType, RawChunk};
-use std::io::{self, Read};
+use std::io::{self, Read, Seek, SeekFrom};
 
 pub(crate) struct ChunkReader<R> {
     r: R,
@@ -15,10 +15,10 @@ impl<R: Read> ChunkReader<R> {
         let length = u32::from_be_bytes(length);
 
         // read chunk type
-        let mut type_ = [0u8; 4];
-        self.r.read_exact(&mut type_)?;
+        let mut ty = [0u8; 4];
+        self.r.read_exact(&mut ty)?;
 
-        crc_hasher.update(&type_);
+        crc_hasher.update(&ty);
 
         // read chunk data
         let mut data = vec![0; length as usize];
@@ -39,10 +39,31 @@ impl<R: Read> ChunkReader<R> {
         }
         Ok(RawChunk {
             length,
-            ty: ChunkType(type_),
+            ty: ChunkType(ty),
             data,
             crc,
         })
+    }
+}
+
+impl<R: Read + Seek> ChunkReader<R> {
+    pub(crate) fn skip_chunk(&mut self) -> io::Result<(ChunkType, usize)> {
+        // read chunk length
+        let mut length = [0u8; 4];
+        self.r.read_exact(&mut length)?;
+        let length = u32::from_be_bytes(length);
+
+        // read chunk type
+        let mut ty = [0u8; 4];
+        self.r.read_exact(&mut ty)?;
+
+        // skip chunk data
+        self.r.seek(SeekFrom::Current(length as i64))?;
+
+        // skip crc sum
+        self.r.seek(SeekFrom::Current(4))?;
+
+        Ok((ChunkType(ty), 4usize + length as usize + 4 + 4))
     }
 }
 
