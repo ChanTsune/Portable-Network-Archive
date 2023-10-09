5265445294b98e7bed0ad93fbb86ad597dbe663b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-10-09 13:56:02 +0900

    :memo: Add doc comment to `MIN_CHUNK_BYTES_SIZE` constant

--- lib/src/chunk.rs
+++ lib/src/chunk.rs
@@ -9,6 +9,7 @@ pub(crate) use self::{read::ChunkReader, write::ChunkWriter};
 pub use self::{traits::*, types::*};
 use std::{mem, ops::Deref};
 
+/// Minimum required size of bytes to represent [`Chunk`].
 pub const MIN_CHUNK_BYTES_SIZE: usize = 12;
 
 pub(crate) trait ChunkExt: Chunk {
