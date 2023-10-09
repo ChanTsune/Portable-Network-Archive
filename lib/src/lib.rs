eb55d09510d06406509bf14492097b2f0aa51590
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-10-09 13:30:41 +0900

    :memo: Add crate level documentation comment

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,11 @@
+//! A library for reading and writing PNA archives
+//!
+//! This library provides utilities necessary to manage PNA archives
+//! abstracted over a reader or writer. Great strides are taken to ensure that
+//! an archive is never required to be fully resident in memory, and all objects
+//! provide largely a streaming interface to read bytes from.
+
+#![doc(html_root_url = "https://docs.rs/libpna/0.3")]
 pub(crate) mod archive;
 pub(crate) mod chunk;
 pub(crate) mod cipher;
