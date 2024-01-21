e36f766aa28c67f9ee4b027fb64d6aab23f1a678
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-01-21 15:31:11 +0900

    :construction_worker: Use `/tmp/` for CI resources output path

--- ci/script/run.sh
+++ ci/script/run.sh
@@ -1,21 +1,28 @@
 set -eu
 
+RESOURCE_DIR="/tmp/"
 TEST_FILE="test_data.txt"
+TEST_FILE_PATH="${RESOURCE_DIR}${TEST_FILE}"
 
 setup() {
-  head -c $((6 * 1024 * 1024 * 1024)) < /dev/urandom > "$TEST_FILE"
+  head -c $((6 * 1024 * 1024 * 1024)) < /dev/urandom > "${TEST_FILE_PATH}"
 }
 
 teardown() {
-  rm -f "$TEST_FILE" "$@"
+  rm -f "${TEST_FILE_PATH}"
+  for FILE in "$@"
+  do
+    rm -f "${RESOURCE_DIR}${FILE}"
+  done
 }
 
 run_with() {
   PNA_FILE="$1"
   OPTIONS=${*:2}
-  pna create $OPTIONS --overwrite "$PNA_FILE" "$TEST_FILE"
-  pna extract --overwrite --out-dir out/ "$PNA_FILE"
-  cmp test_data.txt "out/$TEST_FILE"
+  PNA_FILE_PATH="${RESOURCE_DIR}${PNA_FILE}"
+  pna create ${OPTIONS} --overwrite "${PNA_FILE_PATH}" "${TEST_FILE_PATH}"
+  pna extract --overwrite --out-dir "${RESOURCE_DIR}" "${PNA_FILE_PATH}"
+  cmp "${TEST_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"
 }
 
 main() {
