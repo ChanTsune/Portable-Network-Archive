5348675d97741845dce8a85e4d4e2d1d0f4ec49e
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-02-06 00:00:27 +0900

    :white_check_mark: Reduce use disk space

--- ci/script/run.sh
+++ ci/script/run.sh
@@ -10,10 +10,6 @@ setup() {
 
 teardown() {
   rm -f "${TEST_FILE_PATH}"
-  for FILE in "$@"
-  do
-    rm -f "${RESOURCE_DIR}${FILE}"
-  done
 }
 
 run_with() {
@@ -23,13 +19,14 @@ run_with() {
   pna create ${OPTIONS} --overwrite "${PNA_FILE_PATH}" "${TEST_FILE_PATH}"
   pna extract --overwrite --out-dir "${RESOURCE_DIR}" "${PNA_FILE_PATH}"
   cmp "${TEST_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"
+  rm -rf "${PNA_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"
 }
 
 main() {
   setup
   run_with "store.pna" --store
   run_with "store_solid.pna" --store --solid
-  teardown "store.pna" "store_solid.pna"
+  teardown
 }
 
 main "$@"
