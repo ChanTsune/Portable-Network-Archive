0f2166b8c8a9ea6a89c26cbbb2b85aae037613c1
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-02-21 11:30:49 +0900

    :truck: Move some cli private fs utilities to pna public api

--- cli/src/utils/fs.rs
+++ cli/src/utils/fs.rs
@@ -1,5 +1,6 @@
+pub(crate) use pna::fs::*;
 use std::path::Path;
-use std::{fs, io, os};
+use std::{fs, io};
 
 #[inline]
 pub(crate) fn remove<P: AsRef<Path>>(path: P) -> io::Result<()> {
@@ -8,22 +9,5 @@ pub(crate) fn remove<P: AsRef<Path>>(path: P) -> io::Result<()> {
         fs::remove_dir_all(path)
     } else {
         fs::remove_file(path)
-    }
-}
-
-#[cfg(unix)]
-#[inline]
-pub(crate) fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::Result<()> {
-    os::unix::fs::symlink(original, link)
-}
-
-#[cfg(windows)]
-#[inline]
-pub(crate) fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::Result<()> {
-    let original = original.as_ref();
-    if original.is_dir() {
-        os::windows::fs::symlink_dir(original, link)
-    } else {
-        os::windows::fs::symlink_file(original, link)
     }
 }
