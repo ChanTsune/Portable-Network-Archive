abd367d83ad078957b01d9fafa971815dc8002d5
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-02-28 23:01:40 +0900

    :white_check_mark: Add benchmark split subcommand

---
+++ cli/benches/split.rs
@@ -0,0 +1,60 @@
+#![feature(test)]
+extern crate test;
+
+use clap::Parser;
+use portable_network_archive::{cli, command};
+use test::Bencher;
+
+#[bench]
+fn create_with_split(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(cli::Cli::parse_from([
+            "pna",
+            "--quiet",
+            "c",
+            &format!("{}/store.pna", env!("CARGO_TARGET_TMPDIR")),
+            "--store",
+            "--split",
+            "3MB",
+            "--overwrite",
+            "-r",
+            "../resources/test/raw/",
+        ]))
+        .unwrap()
+    })
+}
+
+#[bench]
+fn split(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(cli::Cli::parse_from([
+            "pna",
+            "--quiet",
+            "experimental",
+            "split",
+            "../resources/test/store.pna",
+            "--overwrite",
+            "--max-size",
+            "3MB",
+            "--out-dir",
+            &format!("{}/split/", env!("CARGO_TARGET_TMPDIR")),
+        ]))
+        .unwrap()
+    })
+}
+
+#[bench]
+fn extract_multipart(b: &mut Bencher) {
+    b.iter(|| {
+        command::entry(cli::Cli::parse_from([
+            "pna",
+            "--quiet",
+            "x",
+            "../resources/test/multipart.part1.pna",
+            "--overwrite",
+            "--out-dir",
+            &format!("{}/multipart/", env!("CARGO_TARGET_TMPDIR")),
+        ]))
+        .unwrap()
+    })
+}
