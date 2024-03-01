580eebf1998a48de9114d7d2bd276cc5b5d42992
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-03-01 22:47:43 +0900

    :boom: `Compression::into` make clamp to each algorithm level range

--- lib/src/compress/xz.rs
+++ lib/src/compress/xz.rs
@@ -3,10 +3,40 @@ pub(crate) type CompressionLevel = u32;
 impl From<crate::CompressionLevel> for CompressionLevel {
     #[inline]
     fn from(value: crate::CompressionLevel) -> Self {
-        if value == crate::CompressionLevel::default() {
+        if value == crate::CompressionLevel::DEFAULT {
             6
         } else {
-            value.0 as Self
+            (value.0 as Self).clamp(0, 9)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn min() {
+        assert_eq!(CompressionLevel::from(crate::CompressionLevel::from(0)), 0);
+    }
+
+    #[test]
+    fn max() {
+        assert_eq!(CompressionLevel::from(crate::CompressionLevel::from(9)), 9);
+    }
+
+    #[test]
+    fn default() {
+        assert_eq!(
+            CompressionLevel::from(crate::CompressionLevel::default()),
+            6
+        );
+    }
+
+    #[test]
+    fn out_of_range() {
+        assert_eq!(
+            CompressionLevel::from(crate::CompressionLevel::from(100)),
+            9
+        );
     }
 }
--- lib/src/compress/zstandard.rs
+++ lib/src/compress/zstandard.rs
@@ -5,9 +5,45 @@ impl From<CompressionLevel> for zstd_safe::CompressionLevel {
     #[inline]
     fn from(value: CompressionLevel) -> Self {
         if value == CompressionLevel::DEFAULT {
-            0
+            zstd::DEFAULT_COMPRESSION_LEVEL
         } else {
-            value.0 as Self
+            (value.0 as Self).clamp(zstd_safe::min_c_level(), zstd_safe::max_c_level())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn min() {
+        assert_eq!(
+            zstd_safe::CompressionLevel::from(CompressionLevel::from(0)),
+            0
+        );
+    }
+
+    #[test]
+    fn max() {
+        assert_eq!(
+            zstd_safe::CompressionLevel::from(CompressionLevel::from(22)),
+            zstd_safe::max_c_level()
+        );
+    }
+
+    #[test]
+    fn default() {
+        assert_eq!(
+            zstd_safe::CompressionLevel::from(CompressionLevel::default()),
+            zstd::DEFAULT_COMPRESSION_LEVEL
+        );
+    }
+
+    #[test]
+    fn out_of_range() {
+        assert_eq!(
+            zstd_safe::CompressionLevel::from(CompressionLevel::from(100)),
+            zstd_safe::max_c_level()
+        );
     }
 }
