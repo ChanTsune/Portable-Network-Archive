34a0f484395a114c4cdfbdb2fe65409629af6a0a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-03-01 22:47:43 +0900

    :boom: `Compression::into` make clamp to each algorithm level range

--- lib/src/compress/deflate.rs
+++ lib/src/compress/deflate.rs
@@ -7,7 +7,44 @@ impl From<CompressionLevel> for Compression {
         if value == CompressionLevel::DEFAULT {
             Self::default()
         } else {
-            Self::new(value.0 as u32)
+            Self::new((value.0 as u32).clamp(Self::none().level(), Self::best().level()))
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn min() {
+        assert_eq!(
+            Compression::from(CompressionLevel::from(0)),
+            Compression::none()
+        );
+    }
+
+    #[test]
+    fn max() {
+        assert_eq!(
+            Compression::from(CompressionLevel::from(9)),
+            Compression::best()
+        );
+    }
+
+    #[test]
+    fn default() {
+        assert_eq!(
+            Compression::from(CompressionLevel::default()),
+            Compression::default()
+        );
+    }
+
+    #[test]
+    fn out_of_range() {
+        assert_eq!(
+            Compression::from(CompressionLevel::from(100)),
+            Compression::best()
+        );
     }
 }
