19196d22e592560c261f3d86facea82e94fa9c44
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-03-16 21:41:25 +0900

    :zap: Support without extension

--- cli/src/utils/path.rs
+++ cli/src/utils/path.rs
@@ -4,11 +4,15 @@ use std::path::{Path, PathBuf};
 pub(crate) fn part_name<P: AsRef<Path>>(p: P, n: usize) -> Option<PathBuf> {
     #[inline]
     fn with_ext(p: &Path, n: usize) -> Option<PathBuf> {
-        let name = p.file_stem()?;
+        let name = PathBuf::from(p.file_stem()?);
         if let Some(ext) = p.extension() {
-            Some(PathBuf::from(name).with_extension(format!("part{n}.{}", ext.to_string_lossy())))
+            if ext.eq_ignore_ascii_case("pna") {
+                Some(name.with_extension(format!("part{n}.{}", ext.to_string_lossy())))
+            } else {
+                Some(name.with_extension(format!("part{n}")))
+            }
         } else {
-            Some(PathBuf::from(name).with_extension(format!("part{n}")))
+            Some(name.with_extension(format!("part{n}")))
         }
     }
     let p = p.as_ref();
@@ -40,6 +44,24 @@ mod tests {
         assert_eq!(
             part_name("parent/a.part1.pna", 2),
             Some(PathBuf::from("parent/a.part2.pna"))
+        );
+    }
+
+    #[test]
+    fn non_part_to_part_without_extension() {
+        assert_eq!(part_name("a", 1), Some(PathBuf::from("a.part1")));
+        assert_eq!(
+            part_name("parent/a", 1),
+            Some(PathBuf::from("parent/a.part1"))
+        );
+    }
+
+    #[test]
+    fn part_to_part_without_extension() {
+        assert_eq!(part_name("a.part1", 2), Some(PathBuf::from("a.part2")));
+        assert_eq!(
+            part_name("parent/a.part1", 2),
+            Some(PathBuf::from("parent/a.part2"))
         );
     }
 }
