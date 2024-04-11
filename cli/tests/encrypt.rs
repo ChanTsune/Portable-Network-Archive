e335ccd422046b967eb49fdfad74611f7b3e50e7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-11 14:43:24 +0900

    :white_check_mark: Add test case for camellia encryption

--- cli/tests/encrypt.rs
+++ cli/tests/encrypt.rs
@@ -13,6 +13,8 @@ fn aes_ctr_archive() {
         "../resources/test/raw",
         "--password",
         "password",
+        "--aes",
+        "ctr",
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
@@ -22,7 +24,97 @@ fn aes_ctr_archive() {
         &format!("{}/zstd_aes_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
         "--overwrite",
         "--out-dir",
-        &env!("CARGO_TARGET_TMPDIR"),
+        &format!("{}/zstd_aes_ctr/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn aes_cbc_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/zstd_aes_cbc.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "cbc",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/zstd_aes_cbc.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/zstd_aes_cbc/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn camellia_ctr_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/zstd_camellia_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--camellia",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/zstd_camellia_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/zstd_camellia_ctr/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn camellia_cbc_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/zstd_camellia_cbc.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "cbc",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/zstd_camellia_cbc.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/zstd_camellia_cbc/", env!("CARGO_TARGET_TMPDIR")),
         "--password",
         "password",
     ]))
