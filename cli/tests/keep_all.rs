079fdb60e27a1455a3d2797ad577121ccfeb98f3
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-13 14:02:13 +0900

    :white_check_mark: Add test for keep xattrs

---
+++ cli/tests/keep_all.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use portable_network_archive::{cli, command};
+
+#[test]
+fn archive_keep_all() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/keep_all.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/keep_all.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/keep_all/", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
