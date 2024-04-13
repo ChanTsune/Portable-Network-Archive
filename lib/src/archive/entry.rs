666e8bba4ea17dc19c93eefcc7495f31e22488b8
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-13 12:42:28 +0900

    :sparkles: Add `SolidArchive` for easily writing solid mode archive

--- lib/src/archive/entry.rs
+++ lib/src/archive/entry.rs
@@ -9,7 +9,7 @@ mod reference;
 mod write;
 
 pub use self::{attr::*, builder::*, header::*, meta::*, name::*, options::*, reference::*};
-use self::{private::*, read::*, write::*};
+pub(crate) use self::{private::*, read::*, write::*};
 use crate::{
     chunk::{chunk_data_split, ChunkExt, ChunkReader, ChunkType, RawChunk, MIN_CHUNK_BYTES_SIZE},
     util::slice::skip_while,
