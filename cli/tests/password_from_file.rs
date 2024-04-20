726fc43e31eb9c4f9d4746696db549e2fbd4699c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-20 21:46:42 +0900

    :white_check_mark: Add test for `--password-file` option

---
+++ cli/tests/password_from_file.rs
@@ -0,0 +1,42 @@
+use clap::Parser;
+use portable_network_archive::{cli, command};
+use std::fs;
+use std::path::PathBuf;
+
+#[test]
+fn archive_password_from_file() {
+    let password_file_path = PathBuf::from(env!("CARGO_TARGET_TMPDIR")).join("password_file");
+    fs::write(&password_file_path, "archive_password_from_file").unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/password_from_file.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password-file",
+        password_file_path.to_str().unwrap(),
+        "--aes",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/password_from_file.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/password_from_file/", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "archive_password_from_file",
+    ]))
+    .unwrap();
+}
