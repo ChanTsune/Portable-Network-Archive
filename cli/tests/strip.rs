1f1a791ed33ce68ef625e873c31a030a8cb4e186
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-21 21:45:29 +0900

    :white_check_mark: Add test for strip subcommand

---
+++ cli/tests/strip.rs
@@ -0,0 +1,49 @@
+use clap::Parser;
+use portable_network_archive::{cli, command};
+
+#[test]
+fn archive_strip_metadata() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/strip_metadata.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "experimental",
+        "strip",
+        &format!("{}/strip_metadata.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/strip_metadata.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/strip_metadata/", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
