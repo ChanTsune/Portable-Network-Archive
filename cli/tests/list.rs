af69728a05f05d381ab0c72d0e4c5035daacb255
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-23 23:25:03 +0900

    :white_check_mark: Add test for list command

---
+++ cli/tests/list.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use portable_network_archive::{cli, command};
+
+#[test]
+fn archive_list() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/list.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "list",
+        "-l",
+        &format!("{}/list.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn archive_list_solid() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/list_solid.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--solid",
+        "--keep-xattr",
+        "--keep-timestamp",
+        "--keep-permission",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "list",
+        "-l",
+        &format!("{}/list_solid.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--solid",
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
