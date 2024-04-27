ad75c24070a7bfb6932fb50c10c98a3d4f9c279c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-27 16:20:42 +0900

    :truck: Move internal module

--- pna/src/ext.rs
+++ pna/src/ext.rs
@@ -1,55 +1,12 @@
+mod archive;
+
+pub use archive::*;
 use libpna::Archive;
-use std::path::Path;
-use std::{fs, io};
+use std::fs;
 
 mod private {
     use super::*;
 
     pub trait Sealed {}
     impl Sealed for Archive<fs::File> {}
-}
-
-pub trait ArchiveFsExt: private::Sealed {
-    fn create<P: AsRef<Path>>(path: P) -> io::Result<Self>
-    where
-        Self: Sized;
-    fn open<P: AsRef<Path>>(path: P) -> io::Result<Self>
-    where
-        Self: Sized;
-}
-
-impl ArchiveFsExt for Archive<fs::File> {
-    /// # Examples
-    /// ```no_run
-    /// # use std::io::{self, prelude::*};
-    /// use pna::prelude::*;
-    /// use pna::Archive;
-    ///
-    /// # fn main() -> io::Result<()> {
-    /// let mut archive = Archive::create("archive.pna")?;
-    /// archive.finalize()?;
-    /// # Ok(())
-    /// # }
-    /// ```
-    fn create<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let file = fs::File::create(path)?;
-        Archive::write_header(file)
-    }
-
-    /// # Examples
-    /// ```no_run
-    /// # use std::io::{self, prelude::*};
-    /// use pna::prelude::*;
-    /// use pna::Archive;
-    ///
-    /// # fn main() -> io::Result<()> {
-    /// let mut archive = Archive::open("archive.pna")?;
-    /// archive.finalize()?;
-    /// # Ok(())
-    /// # }
-    /// ```
-    fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let file = fs::File::open(path)?;
-        Archive::read_header(file)
-    }
 }
