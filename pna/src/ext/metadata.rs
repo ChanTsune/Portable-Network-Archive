5d2ce3e0a412a94181fe7c4ff9c9c0da52ebb9ab
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-05-12 13:42:59 +0900

    :sparkles: Add `with_xxx_time` to `MetadataTimeExt`

--- pna/src/ext/metadata.rs
+++ pna/src/ext/metadata.rs
@@ -6,6 +6,9 @@ pub trait MetadataTimeExt: private::Sealed {
     fn created_time(&self) -> Option<SystemTime>;
     fn modified_time(&self) -> Option<SystemTime>;
     fn accessed_time(&self) -> Option<SystemTime>;
+    fn with_created_time(self, created_time: Option<SystemTime>) -> Self;
+    fn with_modified_time(self, modified_time: Option<SystemTime>) -> Self;
+    fn with_accessed_time(self, accessed_time: Option<SystemTime>) -> Self;
 }
 
 impl MetadataTimeExt for Metadata {
@@ -61,5 +64,59 @@ impl MetadataTimeExt for Metadata {
     /// ```
     fn accessed_time(&self) -> Option<SystemTime> {
         self.accessed().map(|it| SystemTime::UNIX_EPOCH + it)
+    }
+
+    /// Set created time.
+    ///
+    /// # Examples
+    /// ```
+    /// use pna::{prelude::*, Metadata};
+    /// use std::time::SystemTime;
+    ///
+    /// let metadata = Metadata::new().with_created_time(Some(SystemTime::now()));
+    /// ```
+    /// # Panic
+    /// When given created time is before unix epoch, it will be panic.
+    fn with_created_time(self, created_time: Option<SystemTime>) -> Self {
+        self.with_created(created_time.map(|it| {
+            it.duration_since(SystemTime::UNIX_EPOCH)
+                .expect("created time must be after unix epoch")
+        }))
+    }
+
+    /// Set modified time.
+    ///
+    /// # Examples
+    /// ```
+    /// use pna::{prelude::*, Metadata};
+    /// use std::time::SystemTime;
+    ///
+    /// let metadata = Metadata::new().with_modified_time(Some(SystemTime::now()));
+    /// ```
+    /// # Panic
+    /// When given modified time is before unix epoch, it will be panic.
+    fn with_modified_time(self, modified_time: Option<SystemTime>) -> Self {
+        self.with_modified(modified_time.map(|it| {
+            it.duration_since(SystemTime::UNIX_EPOCH)
+                .expect("modified time must be after unix epoch")
+        }))
+    }
+
+    /// Set accessed time.
+    ///
+    /// # Examples
+    /// ```
+    /// use pna::{prelude::*, Metadata};
+    /// use std::time::SystemTime;
+    ///
+    /// let metadata = Metadata::new().with_accessed_time(Some(SystemTime::now()));
+    /// ```
+    /// # Panic
+    /// When given accessed time is before unix epoch, it will be panic.
+    fn with_accessed_time(self, accessed_time: Option<SystemTime>) -> Self {
+        self.with_accessed(accessed_time.map(|it| {
+            it.duration_since(SystemTime::UNIX_EPOCH)
+                .expect("accessed time must be after unix epoch")
+        }))
     }
 }
