b63f38008b27a47a2a7d9670cd42fba8c71f0eb3
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-05-12 15:20:21 +0900

    :memo: Add documentation comment for `pna::prelude`

--- pna/src/prelude.rs
+++ pna/src/prelude.rs
@@ -1,2 +1,11 @@
+//! PNA Prelude.
+//!
+//! The purpose of this module is to alleviate imports of many common PNA traits
+//! by adding a glob import to modules:
+//!
+//! ```
+//! # #![allow(unused_imports)]
+//! use pna::prelude::*;
+//! ```
 pub use crate::ext::ArchiveFsExt;
 pub use libpna::Entry;
