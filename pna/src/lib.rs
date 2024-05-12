8b805b3fca96695c374e9069a921c0b8a5758db7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-05-12 14:21:40 +0900

    :wrench: deny missing_docs for `pna` crate

--- pna/src/lib.rs
+++ pna/src/lib.rs
@@ -4,6 +4,7 @@
 //! necessary to manage PNA archives abstracted over a reader or writer hosted by [libpna].
 #![cfg_attr(target_os = "wasi", feature(wasi_ext))]
 #![doc(html_root_url = "https://docs.rs/pna/0.11.0")]
+#![deny(missing_docs)]
 mod ext;
 pub mod fs;
 pub mod prelude;
