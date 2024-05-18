1e39310566abfbf1e1f0f372e0f2457b46896ed4
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-05-18 12:22:33 +0900

    :white_check_mark: Add check for doc version sync

--- pna/src/lib.rs
+++ pna/src/lib.rs
@@ -10,3 +10,16 @@ pub mod fs;
 pub mod prelude;
 
 pub use libpna::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_readme_deps() {
+        version_sync::assert_markdown_deps_updated!("README.md");
+    }
+
+    #[test]
+    fn test_html_root_url() {
+        version_sync::assert_html_root_url_updated!("src/lib.rs");
+    }
+}
