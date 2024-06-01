b460a42d1139135cdcb077d4b6acf47b7ee63a32
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-06-01 23:40:54 +0900

    :white_check_mark: Add extract macos acl

---
+++ cli/tests/restore_acl.rs
@@ -0,0 +1,19 @@
+#![cfg(feature = "acl")]
+use clap::Parser;
+use portable_network_archive::{cli, command};
+
+#[test]
+fn extract_macos_acl() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        "../resources/test/macos_acl.pna",
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/macos_acl/", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-acl",
+        "--unstable",
+    ]))
+    .unwrap();
+}
