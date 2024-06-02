8bda640c287bd16845376b7063d4a205d7aab19a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-06-02 22:05:16 +0900

    :white_check_mark: linux acl test

--- cli/tests/restore_acl.rs
+++ cli/tests/restore_acl.rs
@@ -3,6 +3,22 @@ use clap::Parser;
 use portable_network_archive::{cli, command};
 
 #[test]
+fn extract_linux_acl() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        "../resources/test/linux_acl.pna",
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/linux_acl/", env!("CARGO_TARGET_TMPDIR")),
+        "--keep-acl",
+        "--unstable",
+    ]))
+    .unwrap();
+}
+
+#[test]
 fn extract_macos_acl() {
     command::entry(cli::Cli::parse_from([
         "pna",
