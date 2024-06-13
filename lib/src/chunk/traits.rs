236b7ffb5eda33c725b5a6d4713db331c28f451d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-06-13 23:46:44 +0900

    :pencil2: Add tailing . to documentation comment in Chunk trait

--- lib/src/chunk/traits.rs
+++ lib/src/chunk/traits.rs
@@ -1,17 +1,17 @@
 use super::{ChunkType, Crc32};
 
-/// The smallest data unit in PNA
+/// The smallest data unit in PNA.
 pub trait Chunk {
-    /// Length of data in bytes
+    /// Length of data in bytes.
     #[inline]
     fn length(&self) -> u32 {
         self.data().len() as u32
     }
-    /// Type of chunk
+    /// Type of chunk.
     fn ty(&self) -> ChunkType;
-    /// Data of chunk
+    /// Data of chunk.
     fn data(&self) -> &[u8];
-    /// CRC32 of chunk type and data
+    /// CRC32 of chunk type and data.
     #[inline]
     fn crc(&self) -> u32 {
         let mut crc = Crc32::new();
