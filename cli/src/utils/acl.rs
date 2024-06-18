ada5f17e250e003dd899fce4d9feb3d73a9740a7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-06-18 21:57:36 +0900

    :truck: move internal module

--- cli/src/utils/acl.rs
+++ cli/src/utils/acl.rs
@@ -1,9 +1,7 @@
 #[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "macos"))]
 mod unix;
-#[cfg(windows)]
-mod windows;
 
+#[cfg(windows)]
+pub use crate::utils::os::windows::acl::*;
 #[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "macos"))]
 pub use unix::*;
-#[cfg(windows)]
-pub use windows::*;
--- cli/src/utils/os/windows.rs
+++ cli/src/utils/os/windows.rs
@@ -1,2 +1,4 @@
+#[cfg(windows)]
+pub mod acl;
 pub(crate) mod fs;
 pub(crate) mod security;
