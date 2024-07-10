333d0d29e351ea8569e9fe5f5a1f101e5a30d2d6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-07-10 21:59:24 +0900

    :zap: Add shorthand for `RegularEntry` header information

--- lib/src/entry.rs
+++ lib/src/entry.rs
@@ -1014,6 +1014,36 @@ impl<T> NormalEntry<T> {
         &self.header
     }
 
+    /// Path of the entry.
+    #[inline]
+    pub fn path(&self) -> &EntryName {
+        &self.header.path
+    }
+
+    /// Type of the entry.
+    #[inline]
+    pub const fn data_kind(&self) -> DataKind {
+        self.header.data_kind
+    }
+
+    /// Compression method of the entry.
+    #[inline]
+    pub const fn compression(&self) -> Compression {
+        self.header.compression
+    }
+
+    /// Encryption method of the entry.
+    #[inline]
+    pub const fn encryption(&self) -> Encryption {
+        self.header.encryption
+    }
+
+    /// Cipher mode of the entry's encryption method.
+    #[inline]
+    pub const fn cipher_mode(&self) -> CipherMode {
+        self.header.cipher_mode
+    }
+
     /// Metadata of the entry.
     #[inline]
     pub fn metadata(&self) -> &Metadata {
