95e4b8ba9f65173ccd9fa9184fcbea9408a6f1ac
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-07-25 22:26:26 +0900

    :white_check_mark: test for cli password hash options

--- ci/script/combination.sh
+++ ci/script/combination.sh
@@ -4,6 +4,7 @@ set -eu
 keepOptions=( "--keep-dir" "--keep-timestamp" "--keep-permission" "--keep-xattr" )
 compressionOptions=( "--store" "--deflate" "--zstd" "--xz" )
 encryptionOptions=( "" "--aes cbc" "--aes ctr" "--camellia cbc" "--camellia ctr" )
+hashOptions=( "" "--argon2" "--pbkdf2")
 solidOption=( "" "--solid" )
 
 for keep in "${keepOptions[@]}"; do
@@ -11,8 +12,10 @@ for keep in "${keepOptions[@]}"; do
     for encrypt in "${encryptionOptions[@]}"; do
       for solid in "${solidOption[@]}"; do
         if [[ "$encrypt" == "--aes cbc" || "$encrypt" == "--aes ctr" || "$encrypt" == "--camellia cbc" || "$encrypt" == "--camellia ctr" ]]; then
-          echo "pna experimental stdio -c $keep $compress $encrypt --password password $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --password password --out-dir '/tmp/$keep$compress$encrypt$solid.pna'"
-          pna experimental stdio -c $keep $compress $encrypt --password password $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --password password --overwrite --out-dir "/tmp/$keep$compress$encrypt$solid.pna"
+          for hash in "${hashOptions[@]}"; do
+            echo "pna experimental stdio -c $keep $compress $encrypt $hash --password password $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --password password --out-dir '/tmp/$keep$compress$encrypt$hash$solid.pna'"
+            pna experimental stdio -c $keep $compress $encrypt $hash --password password $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --password password --overwrite --out-dir "/tmp/$keep$compress$encrypt$hash$solid.pna"
+          done
         else
           echo "pna experimental stdio -c $keep $compress $encrypt $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --out-dir '/tmp/$keep$compress$encrypt$solid.pna'"
           pna experimental stdio -c $keep $compress $encrypt $solid -r . --unstable --exclude ./target/ | pna experimental stdio -x --overwrite --out-dir "/tmp/$keep$compress$encrypt$solid.pna"
