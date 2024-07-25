2623d04dd049ebdd3c1c8f03f747a92e135b1454
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-07-25 22:26:26 +0900

    :white_check_mark: test for cli password hash options

---
+++ cli/tests/password_hash.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use portable_network_archive::{cli, command};
+
+#[test]
+fn aes_ctr_argon2_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/zstd_aes_argon2_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+        "--argon2",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/zstd_aes_argon2_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/zstd_aes_argon2_ctr/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn aes_ctr_pbkdf2_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/zstd_aes_pbkdf2_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+        "--pbkdf2",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/zstd_aes_pbkdf2_ctr.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/zstd_aes_pbkdf2_ctr/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
