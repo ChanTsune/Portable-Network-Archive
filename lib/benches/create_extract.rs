5eea35228970152f16aea2c0e776be33b9253256
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-07-28 23:54:30 +0900

    :white_check_mark: Add bench mark test for *_slice

--- lib/benches/create_extract.rs
+++ lib/benches/create_extract.rs
@@ -2,8 +2,8 @@
 extern crate test;
 
 use libpna::{
-    Archive, CipherMode, Compression, Encryption, EntryBuilder, ReadOptions, WriteOptions,
-    WriteOptionsBuilder,
+    Archive, CipherMode, Compression, Encryption, EntryBuilder, ReadEntry, ReadOptions,
+    WriteOptions, WriteOptionsBuilder,
 };
 use std::io::{self, prelude::*};
 use test::Bencher;
@@ -55,6 +55,37 @@ fn bench_read_archive(b: &mut Bencher, mut options: WriteOptionsBuilder) {
     })
 }
 
+fn bench_read_archive_from_slice(b: &mut Bencher, mut options: WriteOptionsBuilder) {
+    let buf = [24; 1111];
+    let mut writer = Archive::write_header(Vec::with_capacity(10000)).unwrap();
+    writer
+        .add_entry({
+            let mut builder =
+                EntryBuilder::new_file("bench".into(), options.password(Some("password")).build())
+                    .unwrap();
+            builder.write_all(&buf).unwrap();
+            builder.build().unwrap()
+        })
+        .unwrap();
+    let vec = writer.finalize().unwrap();
+
+    b.iter(|| {
+        let mut reader = Archive::read_header_from_slice(vec.as_slice()).unwrap();
+        for item in reader.entries_slice() {
+            let mut buf = Vec::with_capacity(1000);
+            match item.unwrap() {
+                ReadEntry::Solid(_) => (),
+                ReadEntry::Regular(item) => {
+                    item.reader(ReadOptions::with_password(Some("password")))
+                        .unwrap()
+                        .read_to_end(&mut buf)
+                        .unwrap();
+                }
+            }
+        }
+    })
+}
+
 #[bench]
 fn write_store_archive(b: &mut Bencher) {
     bench_write_archive(b, WriteOptions::builder());
@@ -63,6 +94,11 @@ fn write_store_archive(b: &mut Bencher) {
 #[bench]
 fn read_store_archive(b: &mut Bencher) {
     bench_read_archive(b, WriteOptions::builder());
+}
+
+#[bench]
+fn read_store_archive_from_slice(b: &mut Bencher) {
+    bench_read_archive_from_slice(b, WriteOptions::builder());
 }
 
 #[bench]
@@ -77,6 +113,15 @@ fn write_zstd_archive(b: &mut Bencher) {
 #[bench]
 fn read_zstd_archive(b: &mut Bencher) {
     bench_read_archive(b, {
+        let mut builder = WriteOptions::builder();
+        builder.compression(Compression::ZStandard);
+        builder
+    });
+}
+
+#[bench]
+fn read_zstd_archive_from_slice(b: &mut Bencher) {
+    bench_read_archive_from_slice(b, {
         let mut builder = WriteOptions::builder();
         builder.compression(Compression::ZStandard);
         builder
@@ -102,6 +147,15 @@ fn read_deflate_archive(b: &mut Bencher) {
 }
 
 #[bench]
+fn read_deflate_archive_from_slice(b: &mut Bencher) {
+    bench_read_archive_from_slice(b, {
+        let mut builder = WriteOptions::builder();
+        builder.compression(Compression::Deflate);
+        builder
+    });
+}
+
+#[bench]
 fn write_lzma_archive(b: &mut Bencher) {
     bench_write_archive(b, {
         let mut builder = WriteOptions::builder();
@@ -113,6 +167,15 @@ fn write_lzma_archive(b: &mut Bencher) {
 #[bench]
 fn read_lzma_archive(b: &mut Bencher) {
     bench_read_archive(b, {
+        let mut builder = WriteOptions::builder();
+        builder.compression(Compression::XZ);
+        builder
+    });
+}
+
+#[bench]
+fn read_lzma_archive_from_slice(b: &mut Bencher) {
+    bench_read_archive_from_slice(b, {
         let mut builder = WriteOptions::builder();
         builder.compression(Compression::XZ);
         builder
