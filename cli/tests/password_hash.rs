1d5210325f1fc4eae569b781179427db70641c4b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-07-30 14:02:57 +0900

    :white_check_mark: Add test for password hash options with parameters

--- cli/tests/password_hash.rs
+++ cli/tests/password_hash.rs
@@ -33,6 +33,47 @@ fn aes_ctr_argon2_archive() {
 }
 
 #[test]
+fn aes_ctr_argon2_with_params_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!(
+            "{}/zstd_aes_argon2_with_params_ctr.pna",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+        "--argon2",
+        "t=100,m=250,p=2",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!(
+            "{}/zstd_aes_argon2_with_params_ctr.pna",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
+        "--overwrite",
+        "--out-dir",
+        &format!(
+            "{}/zstd_aes_argon2_with_params_ctr/",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
 fn aes_ctr_pbkdf2_archive() {
     command::entry(cli::Cli::parse_from([
         "pna",
@@ -57,6 +98,47 @@ fn aes_ctr_pbkdf2_archive() {
         "--overwrite",
         "--out-dir",
         &format!("{}/zstd_aes_pbkdf2_ctr/", env!("CARGO_TARGET_TMPDIR")),
+        "--password",
+        "password",
+    ]))
+    .unwrap();
+}
+
+#[test]
+fn aes_ctr_pbkdf2_with_params_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!(
+            "{}/zstd_aes_pbkdf2_with_params_ctr.pna",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--password",
+        "password",
+        "--aes",
+        "ctr",
+        "--pbkdf2",
+        "r=1",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!(
+            "{}/zstd_aes_pbkdf2_with_params_ctr.pna",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
+        "--overwrite",
+        "--out-dir",
+        &format!(
+            "{}/zstd_aes_pbkdf2_with_params_ctr/",
+            env!("CARGO_TARGET_TMPDIR")
+        ),
         "--password",
         "password",
     ]))
