98f38bf72fe622b588362395f1ef4ff7380804b7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-01 14:55:12 +0900

    :zap: Introduce `prelude` module for libpna

---
+++ lib/src/prelude.rs
@@ -0,0 +1,10 @@
+//! PNA Prelude.
+//!
+//! The purpose of this module is to alleviate imports of many common PNA traits
+//! by adding a glob import to modules:
+//!
+//! ```
+//! # #![allow(unused_imports)]
+//! use libpna::prelude::*;
+//! ```
+pub use crate::{Chunk, Entry};
