cbb9b41467cdc2fc5e162b88d8ea4085a7323556
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-02 11:07:40 +0900

    :recycle: Rewrite test to tokio-based for async functions

--- lib/examples/async_io.rs
+++ lib/examples/async_io.rs
@@ -1,5 +1,8 @@
 use libpna::{Archive, EntryBuilder, ReadOptions, WriteOptions};
 use std::io;
+use tokio_util::compat::{
+    FuturesAsyncReadCompatExt, FuturesAsyncWriteCompatExt, TokioAsyncReadCompatExt,
+};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -13,14 +16,15 @@ async fn main() -> io::Result<()> {
 }
 
 async fn create(path: String, file_names: &[String]) -> io::Result<()> {
-    let file = async_std::fs::File::create(path).await?;
+    let file = tokio::fs::File::create(path).await?.compat();
     let mut archive = Archive::write_header_async(file).await?;
     for file_name in file_names {
-        let mut file = async_std::fs::File::open(file_name).await?;
+        let mut file = tokio::fs::File::open(file_name).await?;
         let mut entry_builder =
-            EntryBuilder::new_file(file_name.as_str().into(), WriteOptions::builder().build())?;
-        async_std::io::copy(&mut file, &mut entry_builder).await?;
-        let entry = entry_builder.build()?;
+            EntryBuilder::new_file(file_name.as_str().into(), WriteOptions::builder().build())?
+                .compat_write();
+        tokio::io::copy(&mut file, &mut entry_builder).await?;
+        let entry = entry_builder.into_inner().build()?;
         archive.add_entry_async(entry).await?;
     }
     archive.finalize_async().await?;
@@ -28,12 +32,12 @@ async fn create(path: String, file_names: &[String]) -> io::Result<()> {
 }
 
 async fn extract(path: String) -> io::Result<()> {
-    let file = async_std::fs::File::open(path).await?;
+    let file = tokio::fs::File::open(path).await?.compat();
     let mut archive = Archive::read_header_async(file).await?;
     while let Some(entry) = archive.read_entry_async().await? {
-        let mut file = async_std::fs::File::create(entry.header().path().as_path()).await?;
-        let mut reader = entry.reader(ReadOptions::builder().build())?;
-        async_std::io::copy(&mut reader, &mut file).await?;
+        let mut file = tokio::fs::File::create(entry.header().path().as_path()).await?;
+        let mut reader = entry.reader(ReadOptions::builder().build())?.compat();
+        tokio::io::copy(&mut reader, &mut file).await?;
     }
     Ok(())
 }
