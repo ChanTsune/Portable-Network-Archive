d72cd94bc3803f603e0348139d72895cc5d61d87
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-10 21:39:08 +0900

    :white_check_mark: Add test for solid store archive

--- cli/tests/solid_mode.rs
+++ cli/tests/solid_mode.rs
@@ -2,6 +2,32 @@ use clap::Parser;
 use portable_network_archive::{cli, command};
 
 #[test]
+fn solid_store_archive() {
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        &format!("{}/solid_store.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--store",
+        "--overwrite",
+        "-r",
+        "../resources/test/raw",
+        "--solid",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        &format!("{}/solid_store.pna", env!("CARGO_TARGET_TMPDIR")),
+        "--overwrite",
+        "--out-dir",
+        &format!("{}/solid_store/", env!("CARGO_TARGET_TMPDIR")),
+    ]))
+    .unwrap();
+}
+
+#[test]
 fn solid_zstd_archive() {
     command::entry(cli::Cli::parse_from([
         "pna",
