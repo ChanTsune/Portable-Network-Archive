1ee32462d03deb6f8605411d54532534dcf17571
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-14 17:39:25 +0900

    :recycle: Introduce internal struct `StripOptions`

--- cli/src/command/strip.rs
+++ cli/src/command/strip.rs
@@ -3,12 +3,12 @@ use crate::{
     command::{ask_password, commons::run_process_archive_path, Command},
     utils::{self, PathPartExt},
 };
-use clap::{Parser, ValueHint};
+use clap::{Args, Parser, ValueHint};
 use pna::{Archive, Chunk, Metadata};
 use std::{env::temp_dir, fs, io, path::PathBuf};
 
-#[derive(Parser, Clone, Eq, PartialEq, Hash, Debug)]
-pub(crate) struct StripCommand {
+#[derive(Args, Clone, Copy, Eq, PartialEq, Hash, Debug)]
+pub(crate) struct StripOptions {
     #[arg(long, help = "Keep the timestamp of the files")]
     pub(crate) keep_timestamp: bool,
     #[arg(long, help = "Keep the permissions of the files")]
@@ -17,6 +17,12 @@ pub(crate) struct StripCommand {
     pub(crate) keep_xattr: bool,
     #[arg(long, help = "Keep the acl of the files")]
     pub(crate) keep_acl: bool,
+}
+
+#[derive(Parser, Clone, Eq, PartialEq, Hash, Debug)]
+pub(crate) struct StripCommand {
+    #[command(flatten)]
+    pub(crate) strip_options: StripOptions,
     #[arg(long, help = "Output file path", value_hint = ValueHint::AnyPath)]
     pub(crate) output: Option<PathBuf>,
     #[command(flatten)]
@@ -51,19 +57,19 @@ fn strip_metadata(args: StripCommand, _verbosity: Verbosity) -> io::Result<()> {
         |entry| {
             let mut entry = entry?;
             let mut metadata = Metadata::new();
-            if args.keep_permission {
+            if args.strip_options.keep_permission {
                 metadata = metadata.with_permission(entry.metadata().permission().cloned());
             }
-            if args.keep_timestamp {
+            if args.strip_options.keep_timestamp {
                 metadata = metadata.with_accessed(entry.metadata().accessed());
                 metadata = metadata.with_created(entry.metadata().created());
                 metadata = metadata.with_modified(entry.metadata().modified());
             }
             entry = entry.with_metadata(metadata);
-            if !args.keep_xattr {
+            if !args.strip_options.keep_xattr {
                 entry = entry.with_xattrs(&[]);
             }
-            if !args.keep_acl {
+            if !args.strip_options.keep_acl {
                 let filtered = entry
                     .extra_chunks()
                     .iter()
