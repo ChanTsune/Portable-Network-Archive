b81e09c5698dff66ae919ee837ede5d396f9ae24
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-15 12:09:23 +0900

    :white_check_mark: Remove unnecessary encryption in delete test

--- cli/tests/delete.rs
+++ cli/tests/delete.rs
@@ -14,10 +14,6 @@ fn delete_overwrite() {
         "--keep-xattr",
         "--keep-timestamp",
         "--keep-permission",
-        "--password",
-        "password",
-        "--aes",
-        "ctr",
         #[cfg(windows)]
         {
             "--unstable"
@@ -31,8 +27,6 @@ fn delete_overwrite() {
         "delete",
         &format!("{}/delete_overwrite.pna", env!("CARGO_TARGET_TMPDIR")),
         "resources/test/raw/text.txt",
-        "--password",
-        "password",
     ]))
     .unwrap();
 }
@@ -50,10 +44,6 @@ fn delete_output() {
         "--keep-xattr",
         "--keep-timestamp",
         "--keep-permission",
-        "--password",
-        "password",
-        "--aes",
-        "ctr",
         #[cfg(windows)]
         {
             "--unstable"
@@ -67,8 +57,6 @@ fn delete_output() {
         "delete",
         &format!("{}/delete_output.pna", env!("CARGO_TARGET_TMPDIR")),
         "resources/test/raw/text.txt",
-        "--password",
-        "password",
         "--output",
         &format!("{}/delete_output/deleted.pna", env!("CARGO_TARGET_TMPDIR")),
     ]))
@@ -88,10 +76,6 @@ fn delete_output_exclude() {
         "--keep-xattr",
         "--keep-timestamp",
         "--keep-permission",
-        "--password",
-        "password",
-        "--aes",
-        "ctr",
         #[cfg(windows)]
         {
             "--unstable"
@@ -108,8 +92,6 @@ fn delete_output_exclude() {
         "--exclude",
         "resource/test/raw/**",
         "--unstable",
-        "--password",
-        "password",
         "--output",
         &format!(
             "{}/delete_output_exclude/delete_excluded.pna",
