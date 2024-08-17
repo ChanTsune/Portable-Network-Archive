7169aa6dae524773163a8dec31771f5d9f9698b6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-08-17 22:16:35 +0900

    :zap: impl<'a> From<SolidEntry<&'a [u8]>> for SolidEntry<Vec<u8>>

--- lib/src/entry.rs
+++ lib/src/entry.rs
@@ -477,6 +477,18 @@ impl<'a> From<SolidEntry<Cow<'a, [u8]>>> for SolidEntry<Vec<u8>> {
     }
 }
 
+impl<'a> From<SolidEntry<&'a [u8]>> for SolidEntry<Vec<u8>> {
+    #[inline]
+    fn from(value: SolidEntry<&'a [u8]>) -> Self {
+        Self {
+            header: value.header,
+            phsf: value.phsf,
+            data: value.data.into_iter().map(Into::into).collect(),
+            extra: value.extra.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
 impl<T> TryFrom<RawEntry<T>> for SolidEntry<T>
 where
     RawChunk<T>: Chunk,
