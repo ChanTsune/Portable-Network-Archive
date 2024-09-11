9c39b48d33e9a3e3329f6779b73ca52ac800dcf9
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-09-11 09:14:19 +0900

    :sparkles: Add `EntryFsExt::from_path_with`

--- pna/src/ext/entry.rs
+++ pna/src/ext/entry.rs
@@ -8,12 +8,21 @@ pub trait EntryFsExt: private::Sealed {
     fn from_path<P: AsRef<Path>>(path: P) -> io::Result<Self>
     where
         Self: Sized;
+    /// Create Entry from a given path with option.
+    fn from_path_with<P: AsRef<Path>>(path: P, options: WriteOptions) -> io::Result<Self>
+    where
+        Self: Sized;
 }
 
 impl EntryFsExt for RegularEntry {
     /// Create Entry from a given path.
     ///
+    /// # Errors
+    ///
+    /// Returns an error if an I/O error occurs while creating entry.
+    ///
     /// # Examples
+    ///
     /// ```no_run
     /// # use std::io::{self, prelude::*};
     /// use pna::prelude::*;
@@ -24,20 +33,45 @@ impl EntryFsExt for RegularEntry {
     /// # Ok(())
     /// # }
     /// ```
+    #[inline]
     fn from_path<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        fn inner(path: &Path) -> io::Result<RegularEntry> {
-            let meta = fs::metadata(path)?;
-            let name = path.try_into().map_err(io::Error::other)?;
-            if meta.is_file() {
-                let mut file = fs::File::open(path)?;
-                let mut builder = EntryBuilder::new_file(name, WriteOptions::builder().build())?;
-                io::copy(&mut file, &mut builder)?;
-                builder.build()
-            } else {
-                let builder = EntryBuilder::new_dir(name);
-                builder.build()
-            }
+        Self::from_path_with(path.as_ref(), WriteOptions::builder().build())
+    }
+
+    /// Create Entry from a given path with option.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an I/O error occurs while creating entry.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use std::io::{self, prelude::*};
+    /// use pna::prelude::*;
+    /// use pna::{RegularEntry, WriteOptions};
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// RegularEntry::from_path_with("path/to/file", WriteOptions::store())?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    #[inline]
+    fn from_path_with<P: AsRef<Path>>(path: P, options: WriteOptions) -> io::Result<Self>
+    where
+        Self: Sized,
+    {
+        let path = path.as_ref();
+        let meta = fs::metadata(path)?;
+        let name = path.try_into().map_err(io::Error::other)?;
+        if meta.is_file() {
+            let mut file = fs::File::open(path)?;
+            let mut builder = EntryBuilder::new_file(name, options)?;
+            io::copy(&mut file, &mut builder)?;
+            builder.build()
+        } else {
+            let builder = EntryBuilder::new_dir(name);
+            builder.build()
         }
-        inner(path.as_ref())
     }
 }
