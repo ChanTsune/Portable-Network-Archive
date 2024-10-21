1a5a1e162afc849de3e50fd5dbfc3f36218567be
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-10-21 19:11:19 +0900

    :recycle: use `rand_chacha::ChaCha20Rng` instead of `and::rngs::OsRng`

--- lib/src/random.rs
+++ lib/src/random.rs
@@ -1,9 +1,10 @@
 use password_hash::SaltString;
-use rand::{rngs::OsRng, RngCore};
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
 use std::io;
 
 pub(crate) fn random_bytes(dist: &mut [u8]) -> io::Result<()> {
-    let mut rand = OsRng;
+    let mut rand = ChaCha20Rng::from_entropy();
     rand.try_fill_bytes(dist).map_err(io::Error::other)
 }
 
@@ -14,5 +15,5 @@ pub(crate) fn random_vec(size: usize) -> io::Result<Vec<u8>> {
 }
 
 pub(crate) fn salt_string() -> SaltString {
-    SaltString::generate(OsRng)
+    SaltString::generate(ChaCha20Rng::from_entropy())
 }
