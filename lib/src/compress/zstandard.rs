d1912b53a2f3632b7512525a097f400c484765fb
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-12-04 09:27:44 +0900

    :zap: Introduce internal type alias `ZstdCompressionLevel`

--- lib/src/compress/zstandard.rs
+++ lib/src/compress/zstandard.rs
@@ -2,7 +2,9 @@ use crate::entry::CompressionLevelImpl;
 use crate::CompressionLevel;
 use zstd::zstd_safe;
 
-impl From<CompressionLevel> for zstd_safe::CompressionLevel {
+pub(crate) type ZstdCompressionLevel = zstd_safe::CompressionLevel;
+
+impl From<CompressionLevel> for ZstdCompressionLevel {
     #[inline]
     fn from(value: CompressionLevel) -> Self {
         match value.0 {
@@ -21,16 +23,13 @@ mod tests {
     use super::*;
     #[test]
     fn min() {
-        assert_eq!(
-            zstd_safe::CompressionLevel::from(CompressionLevel::from(0)),
-            0
-        );
+        assert_eq!(ZstdCompressionLevel::from(CompressionLevel::from(0)), 0);
     }
 
     #[test]
     fn max() {
         assert_eq!(
-            zstd_safe::CompressionLevel::from(CompressionLevel::from(22)),
+            ZstdCompressionLevel::from(CompressionLevel::from(22)),
             zstd_safe::max_c_level()
         );
     }
@@ -38,7 +37,7 @@ mod tests {
     #[test]
     fn default() {
         assert_eq!(
-            zstd_safe::CompressionLevel::from(CompressionLevel::default()),
+            ZstdCompressionLevel::from(CompressionLevel::default()),
             zstd::DEFAULT_COMPRESSION_LEVEL
         );
     }
@@ -46,7 +45,7 @@ mod tests {
     #[test]
     fn out_of_range() {
         assert_eq!(
-            zstd_safe::CompressionLevel::from(CompressionLevel::from(100)),
+            ZstdCompressionLevel::from(CompressionLevel::from(100)),
             zstd_safe::max_c_level()
         );
     }
