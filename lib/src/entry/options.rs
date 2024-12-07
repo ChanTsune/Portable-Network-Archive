268054c2db5156289cef6725884d979514d795bc
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-12-07 12:01:48 +0900

    :recycle: `WriteOptions` fields make module private

--- lib/src/entry/options.rs
+++ lib/src/entry/options.rs
@@ -384,8 +384,8 @@ impl TryFrom<u8> for DataKind {
 /// Options for writing an entry.
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct WriteOptions {
-    pub(crate) compress: Compress,
-    pub(crate) cipher: Option<Cipher>,
+    compress: Compress,
+    cipher: Option<Cipher>,
 }
 
 impl WriteOptions {
