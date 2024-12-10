15db87395d4a69750fb04f0d226f54c9911f0920
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-12-10 20:21:46 +0900

    :recycle: Swap caller and callee roles for const context

--- lib/src/entry/reference.rs
+++ lib/src/entry/reference.rs
@@ -83,7 +83,7 @@ impl EntryReference {
     /// ```
     #[inline]
     pub fn as_os_str(&self) -> &OsStr {
-        self.as_ref()
+        self.0.as_ref()
     }
 
     /// Coerces to a [`Path`] slice.
@@ -99,12 +99,12 @@ impl EntryReference {
     /// ```
     #[inline]
     pub fn as_path(&self) -> &Path {
-        self.as_ref()
+        self.0.as_ref()
     }
 
     #[inline]
     pub(crate) fn as_bytes(&self) -> &[u8] {
-        self.as_str().as_bytes()
+        self.0.as_bytes()
     }
 
     /// Create an [`EntryReference`] from a struct impl <code>[Into]<[PathBuf]></code>.
@@ -215,21 +215,21 @@ impl Display for EntryReference {
 impl AsRef<str> for EntryReference {
     #[inline]
     fn as_ref(&self) -> &str {
-        &self.0
+        self.as_str()
     }
 }
 
 impl AsRef<OsStr> for EntryReference {
     #[inline]
     fn as_ref(&self) -> &OsStr {
-        self.0.as_ref()
+        self.as_os_str()
     }
 }
 
 impl AsRef<Path> for EntryReference {
     #[inline]
     fn as_ref(&self) -> &Path {
-        self.0.as_ref()
+        self.as_path()
     }
 }
 
