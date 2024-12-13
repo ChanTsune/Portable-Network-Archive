2f44251eae3d885dcf57bfc33a2ecec29b6465ed
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-12-14 00:04:47 +0900

    :bug: Fixed failed to run on wasmtime about using multithread related things

--- cli/src/utils/thread.rs
+++ cli/src/utils/thread.rs
@@ -2,7 +2,11 @@ use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::io;
 
 pub(crate) fn build() -> io::Result<ThreadPool> {
-    ThreadPoolBuilder::default()
-        .build()
-        .map_err(io::Error::other)
+    if cfg!(target_os = "wasi") {
+        ThreadPoolBuilder::default().use_current_thread()
+    } else {
+        ThreadPoolBuilder::default()
+    }
+    .build()
+    .map_err(io::Error::other)
 }
