64668570c3b1865a82847e17a821d7c27d5dedfb
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-12-25 22:20:27 +0900

    :truck: Move internal function module

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,24 +6,10 @@ mod utils;
 
 use clap::Parser;
 use command::Command;
-use log::Level;
 use std::io;
 
 fn main() -> io::Result<()> {
     let cli = cli::Cli::parse();
-    init_logger(cli.verbosity.log_level_filter())?;
+    cli.init_logger()?;
     cli.execute()
-}
-
-fn init_logger(level: log::LevelFilter) -> io::Result<()> {
-    let base = fern::Dispatch::new();
-    let stderr = fern::Dispatch::new()
-        .level(level)
-        .format(|out, msg, rec| match rec.level() {
-            Level::Error => out.finish(format_args!("error: {}", msg)),
-            Level::Warn => out.finish(format_args!("warning: {}", msg)),
-            Level::Info | Level::Debug | Level::Trace => out.finish(*msg),
-        })
-        .chain(io::stderr());
-    base.chain(stderr).apply().map_err(io::Error::other)
 }
