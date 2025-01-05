4ce4d0f4964b4bf771d36bfee6d35fccc4c6b90c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-05 10:48:36 +0900

    :white_check_mark: Add `copy_dir_all` for test uttility

--- cli/tests/cli/utils.rs
+++ cli/tests/cli/utils.rs
@@ -1,6 +1,23 @@
 pub mod diff;
 
+use std::path::Path;
+use std::{fs, io};
+
 pub fn setup() {
     #[cfg(target_os = "wasi")]
     std::env::set_current_dir(env!("CARGO_MANIFEST_DIR")).expect("Failed to set current dir");
+}
+
+pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
