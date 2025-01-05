a3e3700e2c64c5f683c2f835870f65d6a803a0a7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-05 15:50:44 +0900

    :white_check_mark: strictly check split result

--- cli/tests/cli/split.rs
+++ cli/tests/cli/split.rs
@@ -1,38 +1,69 @@
-use crate::utils::setup;
+use crate::utils::{components_count, copy_dir_all, diff::diff, setup};
 use clap::Parser;
 use portable_network_archive::{cli, command};
+use std::fs;
 
 #[test]
 fn split_archive() {
     setup();
+    copy_dir_all(
+        "../resources/test/raw/",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/in/"),
+    )
+    .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "create",
-        &format!("{}/split.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/split.pna"),
         "--overwrite",
         "-r",
-        "../resources/test/raw/",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/in/"),
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "split",
-        &format!("{}/split.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/split.pna"),
         "--overwrite",
         "--max-size",
         "100kb",
+        "--out-dir",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/split/"),
     ]))
     .unwrap();
+
+    // check split file size
+    for entry in fs::read_dir(concat!(
+        env!("CARGO_TARGET_TMPDIR"),
+        "/split_archive/split/"
+    ))
+    .unwrap()
+    {
+        assert!(fs::metadata(entry.unwrap().path()).unwrap().len() <= 100 * 1000);
+    }
+
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/split.part1.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/split_archive/split/split.part1.pna"
+        ),
         "--overwrite",
         "--out-dir",
-        &format!("{}/split/", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/out/"),
+        "--strip-components",
+        &components_count(concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/in/")).to_string(),
     ]))
+    .unwrap();
+
+    // check completely extracted
+    diff(
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/in/"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/split_archive/out/"),
+    )
     .unwrap();
 }
