b25e4cc072749fb9cf3eb0cb01925e479ee4c795
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-13 11:47:16 +0900

    :white_check_mark: More strictly check cli append test

--- cli/tests/cli/append.rs
+++ cli/tests/cli/append.rs
@@ -1,52 +1,87 @@
-use crate::utils::setup;
+use crate::utils::{components_count, copy_dir_all, diff::diff, setup};
 use clap::Parser;
 use portable_network_archive::{cli, command};
+use std::fs;
 
 #[test]
 fn archive_append() {
     setup();
-    command::entry(cli::Cli::parse_from([
-        "pna",
-        "--quiet",
-        "c",
-        &format!("{}/append.pna", env!("CARGO_TARGET_TMPDIR")),
-        "--overwrite",
-        "-r",
+    copy_dir_all(
         "../resources/test/raw",
-    ]))
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/"),
+    )
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
-        "append",
-        &format!("{}/append.pna", env!("CARGO_TARGET_TMPDIR")),
+        "c",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/append.pna"),
+        "--overwrite",
+        "-r",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/"),
+    ]))
+    .unwrap();
+
+    // Copy extra input
+    fs::copy(
         "../resources/test/store.pna",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/store.pna"),
+    )
+    .unwrap();
+    fs::copy(
         "../resources/test/zstd.pna",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/zstd.pna"),
+    )
+    .unwrap();
+
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "append",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/append.pna"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/store.pna"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/zstd.pna"),
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/append.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/append.pna"),
         "--overwrite",
         "--out-dir",
-        &format!("{}/append/", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/out/"),
+        "--strip-components",
+        &components_count(concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/")).to_string(),
     ]))
+    .unwrap();
+    // check completely extracted
+    diff(
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/in/"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append/out/"),
+    )
     .unwrap();
 }
 
 #[test]
 fn archive_append_split() {
     setup();
+    copy_dir_all(
+        "../resources/test/raw",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append_split/in/"),
+    )
+    .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "c",
-        &format!("{}/append_split.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/append_split.pna"
+        ),
         "--overwrite",
         "-r",
-        "../resources/test/raw",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append_split/in/"),
         "--split",
         "100kib",
         #[cfg(windows)]
@@ -55,23 +90,66 @@ fn archive_append_split() {
         },
     ]))
     .unwrap();
+
+    // Copy extra input
+    fs::copy(
+        "../resources/test/store.pna",
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/in/store.pna"
+        ),
+    )
+    .unwrap();
+    fs::copy(
+        "../resources/test/zstd.pna",
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/in/zstd.pna"
+        ),
+    )
+    .unwrap();
+
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "append",
-        &format!("{}/append_split.part1.pna", env!("CARGO_TARGET_TMPDIR")),
-        "../resources/test/store.pna",
-        "../resources/test/zstd.pna",
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/append_split.part1.pna"
+        ),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/in/store.pna"
+        ),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/in/zstd.pna"
+        ),
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/append_split.part1.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/append_split.part1.pna"
+        ),
         "--overwrite",
         "--out-dir",
-        &format!("{}/append_split/", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append_split/out/"),
+        "--strip-components",
+        &components_count(concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_append_split/out/"
+        ))
+        .to_string(),
     ]))
+    .unwrap();
+    // check completely extracted
+    diff(
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append_split/in/"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_append_split/out/"),
+    )
     .unwrap();
 }
