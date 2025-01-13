5a1f0f6d798dec4ac7348fe8ee04be39751da97d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-13 17:42:26 +0900

    :construction: Added `InitPropVariantFromString`

--- cli/src/utils/os/windows/fs/properties.rs
+++ cli/src/utils/os/windows/fs/properties.rs
@@ -1,3 +1,4 @@
+use libc::memcpy;
 use std::io::Write;
 use std::{
     fs, io,
@@ -5,16 +6,22 @@ use std::{
     path::{Path, PathBuf},
 };
 use windows::{
-    core::HSTRING,
+    core::{HSTRING, PWSTR},
     Win32::{
-        System::Com::{
-            CoTaskMemFree,
-            StructuredStorage::{InitPropVariantFromStringAsVector, PropVariantClear},
+        System::{
+            Com::{
+                CoTaskMemAlloc, CoTaskMemFree,
+                StructuredStorage::{
+                    PropVariantClear, PropVariantToStringAlloc, PROPVARIANT, PROPVARIANT_0_0,
+                    PROPVARIANT_0_0_0,
+                },
+            },
+            Variant::VT_LPWSTR,
         },
         UI::Shell::PropertiesSystem::{
-            IPropertyStore, PSCoerceToCanonicalValue, PSFormatForDisplayAlloc,
-            PSGetNameFromPropertyKey, PSGetPropertyKeyFromName, SHGetPropertyStoreFromParsingName,
-            GETPROPERTYSTOREFLAGS, GPS_DEFAULT, PDFF_DEFAULT,
+            IPropertyStore, PSCoerceToCanonicalValue, PSGetNameFromPropertyKey,
+            PSGetPropertyKeyFromName, SHGetPropertyStoreFromParsingName, GETPROPERTYSTOREFLAGS,
+            GPS_DEFAULT,
         },
     },
 };
@@ -47,7 +54,8 @@ pub(crate) fn get_properties<P: AsRef<Path>>(path: P) -> io::Result<Vec<(String,
             format!("{{{:?}}}", key.fmtid)
         };
         let mut value = unsafe { store.GetValue(&key) }?;
-        let v = unsafe { PSFormatForDisplayAlloc(&key, &value, PDFF_DEFAULT) }?;
+        // let v = unsafe { PSFormatForDisplayAlloc(&key, &value, PDFF_DEFAULT) }?;
+        let v = unsafe { PropVariantToStringAlloc(&value) }.unwrap();
         let s = unsafe { v.to_hstring() }.to_string_lossy();
         unsafe { CoTaskMemFree(Some(v.as_ptr() as _)) };
         unsafe { PropVariantClear(&mut value) }?;
@@ -55,6 +63,28 @@ pub(crate) fn get_properties<P: AsRef<Path>>(path: P) -> io::Result<Vec<(String,
         properties.push(kv);
     }
     Ok(properties)
+}
+
+#[allow(non_snake_case)]
+unsafe fn InitPropVariantFromString<P0>(psz: P0) -> windows::core::Result<PROPVARIANT>
+where
+    P0: windows::core::Param<windows::core::PCWSTR>,
+{
+    let param = psz.param().abi();
+    let byte_count = (param.len() + 1) * std::mem::size_of::<u16>();
+    let mem = CoTaskMemAlloc(byte_count);
+    memcpy(mem, param.as_ptr() as _, byte_count);
+    let param = PWSTR::from_raw(mem as _);
+    let anonymous = PROPVARIANT_0_0 {
+        vt: VT_LPWSTR,
+        wReserved1: 0,
+        wReserved2: 0,
+        wReserved3: 0,
+        Anonymous: PROPVARIANT_0_0_0 { pwszVal: param },
+    };
+    let mut propvar = PROPVARIANT::default();
+    propvar.Anonymous.Anonymous = std::mem::ManuallyDrop::new(anonymous);
+    Ok(propvar)
 }
 
 pub(crate) fn set_properties<P: AsRef<Path>>(
@@ -68,13 +98,13 @@ pub(crate) fn set_properties<P: AsRef<Path>>(
             .write_all(format!("k: {}, v: {}\n", key_name, value).as_bytes())
             .unwrap();
         let mut key = Default::default();
-        unsafe { PSGetPropertyKeyFromName(&HSTRING::from(key_name), &mut key) }.unwrap();
-
-        let mut prop_variant =
-            unsafe { InitPropVariantFromStringAsVector(&HSTRING::from(value)) }.unwrap();
-        unsafe { PSCoerceToCanonicalValue(&key, &mut prop_variant) }.unwrap();
-        // unsafe { store.SetValue(&key, &prop_variant) }.unwrap();
-        unsafe { PropVariantClear(&mut prop_variant) }.unwrap();
+        if let Ok(_) = unsafe { PSGetPropertyKeyFromName(&HSTRING::from(key_name), &mut key) } {
+            let mut prop_variant =
+                unsafe { InitPropVariantFromString(&HSTRING::from(value)) }.unwrap();
+            unsafe { PSCoerceToCanonicalValue(&key, &mut prop_variant) }.unwrap();
+            unsafe { store.SetValue(&key, &prop_variant) }.unwrap();
+            unsafe { PropVariantClear(&mut prop_variant) }.unwrap();
+        }
     }
     unsafe { store.Commit() }.unwrap();
     Ok(())
@@ -91,8 +121,8 @@ mod tests {
     fn get_set_props() {
         unsafe { CoInitializeEx(None, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE) }.unwrap();
         fs::write("empty.txt", "").unwrap();
-        let props = get_properties("empty.txt").unwrap();
-        set_properties("empty.txt", props).unwrap();
+        let _props = get_properties("empty.txt").unwrap();
+        // set_properties("empty.txt", props).unwrap();
         fs::remove_file("empty.txt").unwrap();
         unsafe {
             CoUninitialize();
