b50a59cf08ae186c1ff01fbcfb5949595f36a127
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-20 22:47:10 +0900

    :white_check_mark: more strictly check in test case `user_group`

--- cli/tests/cli/user_group.rs
+++ cli/tests/cli/user_group.rs
@@ -1,19 +1,33 @@
 #![cfg(unix)]
-use crate::utils::setup;
+use crate::utils::{components_count, diff::diff, setup, TestResources};
 use clap::Parser;
 use portable_network_archive::{cli, command};
 
 #[test]
 fn archive_create_uname_gname() {
     setup();
+    TestResources::extract_in(
+        "raw/",
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/in/"
+        ),
+    )
+    .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "c",
-        &format!("{}/create_uname_gname.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/create_uname_gname.pna"
+        ),
         "--overwrite",
         "-r",
-        "../resources/test/raw",
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/in/"
+        ),
         "--keep-permission",
         "--uname",
         "test_user",
@@ -25,33 +39,68 @@ fn archive_create_uname_gname() {
         "pna",
         "ls",
         "-lh",
-        &format!("{}/create_uname_gname.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/create_uname_gname.pna"
+        ),
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/create_uname_gname.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/create_uname_gname.pna"
+        ),
         "--overwrite",
         "--out-dir",
-        &format!("{}/create_uname_gname/", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/out/"
+        ),
         "--keep-permission",
+        "--strip-components",
+        &components_count(concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/in/"
+        ))
+        .to_string(),
     ]))
     .unwrap();
+
+    diff(
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/in/"
+        ),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uname_gname/out/"
+        ),
+    )
+    .unwrap()
 }
 
 #[test]
 fn archive_create_uid_gid() {
     setup();
+    TestResources::extract_in(
+        "raw/",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_create_uid_gid/in/"),
+    )
+    .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "c",
-        &format!("{}/create_uid_gid.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uid_gid/create_uid_gid.pna"
+        ),
         "--overwrite",
         "-r",
-        "../resources/test/raw",
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_create_uid_gid/in/"),
         "--keep-permission",
         "--uid",
         "0",
@@ -64,18 +113,36 @@ fn archive_create_uid_gid() {
         "ls",
         "-lh",
         "--numeric-owner",
-        &format!("{}/create_uid_gid.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uid_gid/create_uid_gid.pna"
+        ),
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/create_uid_gid.pna", env!("CARGO_TARGET_TMPDIR")),
+        concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uid_gid/create_uid_gid.pna"
+        ),
         "--overwrite",
         "--out-dir",
-        &format!("{}/create_uid_gid/", env!("CARGO_TARGET_TMPDIR")),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_create_uid_gid/out/"),
         "--keep-permission",
+        "--strip-components",
+        &components_count(concat!(
+            env!("CARGO_TARGET_TMPDIR"),
+            "/archive_create_uid_gid/in/"
+        ))
+        .to_string(),
     ]))
+    .unwrap();
+
+    diff(
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_create_uid_gid/in/"),
+        concat!(env!("CARGO_TARGET_TMPDIR"), "/archive_create_uid_gid/out/"),
+    )
     .unwrap();
 }
