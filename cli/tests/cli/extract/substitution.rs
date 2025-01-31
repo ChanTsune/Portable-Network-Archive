8d14063ef256cf5831f811840fb032e1e4f56507
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-01-31 17:17:31 +0900

    :white_check_mark: Added test for extract subcommand with --substitution

---
+++ cli/tests/cli/extract/substitution.rs
@@ -0,0 +1,41 @@
+use crate::utils::{diff::diff, setup, TestResources};
+use clap::Parser;
+use portable_network_archive::{cli, command};
+use std::fs;
+
+#[test]
+fn extract_with_substitution() {
+    setup();
+    TestResources::extract_in("raw/", "extract_with_substitution/in/").unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        "extract_with_substitution/extract_with_substitution.pna",
+        "--overwrite",
+        "-r",
+        "extract_with_substitution/in/",
+    ]))
+    .unwrap();
+    assert!(fs::exists("extract_with_substitution/extract_with_substitution.pna").unwrap());
+
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        "extract_with_substitution/extract_with_substitution.pna",
+        "--overwrite",
+        "--out-dir",
+        "extract_with_substitution/out/",
+        "-s",
+        "#extract_with_substitution/in/##",
+        "--unstable",
+    ]))
+    .unwrap();
+
+    diff(
+        "extract_with_substitution/in/",
+        "extract_with_substitution/out/",
+    )
+    .unwrap();
+}
