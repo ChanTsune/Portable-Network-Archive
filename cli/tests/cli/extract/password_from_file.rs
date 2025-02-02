d47274868a23ef0e5edde55131c51b9f45cefd5e
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-02 14:33:31 +0900

    :white_check_mark: Added test for extract with --password-from

---
+++ cli/tests/cli/extract/password_from_file.rs
@@ -0,0 +1,49 @@
+use crate::utils::{diff::diff, setup, TestResources};
+use clap::Parser;
+use portable_network_archive::{cli, command};
+use std::fs;
+
+#[test]
+fn extract_with_password_file() {
+    setup();
+    TestResources::extract_in("raw/", "extract_with_password_file/in/").unwrap();
+    let password_file_path = "extract_with_password_file/password_file";
+    let password = "extract_with_password_file";
+    fs::write(password_file_path, password).unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        "extract_with_password_file/password_from_file.pna",
+        "--overwrite",
+        "-r",
+        "extract_with_password_file/in/",
+        "--password",
+        password,
+        "--aes",
+        "ctr",
+        "--argon2",
+        "t=1,m=50",
+    ]))
+    .unwrap();
+    command::entry(cli::Cli::parse_from([
+        "pna",
+        "--quiet",
+        "x",
+        "extract_with_password_file/password_from_file.pna",
+        "--overwrite",
+        "--out-dir",
+        "extract_with_password_file/out/",
+        "--password-file",
+        password_file_path,
+        "--strip-components",
+        "2",
+    ]))
+    .unwrap();
+
+    diff(
+        "extract_with_password_file/in/",
+        "extract_with_password_file/out/",
+    )
+    .unwrap();
+}
