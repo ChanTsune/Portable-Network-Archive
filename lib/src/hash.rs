7331765cd75b4f4838e9486c35f0f40afa177190
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-04 23:52:50 +0900

    Revert ":alien: Migrate rand from 0.8.5 to 0.9.0 and rand_chacha from 0.3.1 to 0.9.0"
    This reverts commit 8ef399c15f67ca8f3a40ce51b5acd015a02b08d0.

--- lib/src/hash.rs
+++ lib/src/hash.rs
@@ -89,7 +89,7 @@ mod tests {
 
     #[test]
     fn verify_argon2() {
-        let salt = random::salt_string().unwrap();
+        let salt = random::salt_string();
         let mut ph = argon2_with_salt(
             b"pass",
             argon2::Algorithm::Argon2id,
@@ -109,7 +109,7 @@ mod tests {
 
     #[test]
     fn verify_pbkdf2() {
-        let salt = random::salt_string().unwrap();
+        let salt = random::salt_string();
         let mut ph = pbkdf2_with_salt(
             b"pass",
             pbkdf2::Algorithm::Pbkdf2Sha256,
