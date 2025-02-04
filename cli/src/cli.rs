5d4b9ec2248711964f3ff12db7ecd781bb128adb
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-04 18:30:05 +0900

    :sparkles: Added `bug-report` subcommand for assist bug reporting to cli

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -1,7 +1,7 @@
 use crate::command::{
-    append::AppendCommand, complete::CompleteCommand, concat::ConcatCommand, create::CreateCommand,
-    experimental::ExperimentalCommand, extract::ExtractCommand, list::ListCommand,
-    split::SplitCommand, strip::StripCommand,
+    append::AppendCommand, bugreport::BugReportCommand, complete::CompleteCommand,
+    concat::ConcatCommand, create::CreateCommand, experimental::ExperimentalCommand,
+    extract::ExtractCommand, list::ListCommand, split::SplitCommand, strip::StripCommand,
 };
 use clap::{value_parser, ArgGroup, Parser, Subcommand, ValueEnum, ValueHint};
 use log::{Level, LevelFilter};
@@ -80,6 +80,8 @@ pub(crate) enum Commands {
     Strip(StripCommand),
     #[command(about = "Generate shell auto complete")]
     Complete(CompleteCommand),
+    #[command(about = "Generate bug report template")]
+    BugReport(BugReportCommand),
     #[command(about = "Unstable experimental commands")]
     Experimental(ExperimentalCommand),
 }
---
+++ cli/src/command/bugreport.rs
@@ -0,0 +1,19 @@
+use crate::command::Command;
+use bugreport::{bugreport, collector::*, format::Markdown};
+use clap::Parser;
+
+#[derive(Parser, Clone, Eq, PartialEq, Hash, Debug)]
+pub(crate) struct BugReportCommand;
+
+impl Command for BugReportCommand {
+    #[inline]
+    fn execute(self) -> std::io::Result<()> {
+        bugreport!()
+            .info(SoftwareVersion::default())
+            .info(OperatingSystem::default())
+            .info(CommandLine::default())
+            .info(CompileTimeInformation::default())
+            .print::<Markdown>();
+        Ok(())
+    }
+}
