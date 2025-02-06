ff4abcce603da626d7ebedfd273659dbcd96478b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-06 17:32:14 +0900

    :sparkles: Added `--no-recursive(--no-recursion)` option for inverse option of `--recursive(--recursion)` to creation subcommand

--- cli/src/command/append.rs
+++ cli/src/command/append.rs
@@ -37,6 +37,7 @@ use std::{fs::File, io};
     group(ArgGroup::new("store-numeric-owner").args(["numeric_owner"]).requires("keep_permission")),
     group(ArgGroup::new("user-flag").args(["numeric_owner", "uname"])),
     group(ArgGroup::new("group-flag").args(["numeric_owner", "gname"])),
+    group(ArgGroup::new("recursive-flag").args(["recursive", "no_recursive"])),
 )]
 #[cfg_attr(windows, command(
     group(ArgGroup::new("windows-unstable-keep-permission").args(["keep_permission"]).requires("unstable")),
@@ -49,6 +50,12 @@ pub(crate) struct AppendCommand {
         help = "Add the directory to the archive recursively"
     )]
     pub(crate) recursive: bool,
+    #[arg(
+        long,
+        visible_alias = "no-recursion",
+        help = "Do not recursively add directories to the archives. This is the inverse option of --recursive"
+    )]
+    no_recursive: bool,
     #[arg(long, help = "Archiving the directories")]
     pub(crate) keep_dir: bool,
     #[arg(
