cd2975bf63da0fe68e12ebf190bc449d8aaba850
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-06 23:39:48 +0900

    :alembic: Added `--files-from` and `--files-from-stdin` option to `delete` subcommand

--- cli/src/command/delete.rs
+++ cli/src/command/delete.rs
@@ -14,12 +14,19 @@ use std::{io, path::PathBuf};
 
 #[derive(Parser, Clone, Eq, PartialEq, Hash, Debug)]
 #[command(
+    group(ArgGroup::new("unstable-files-from").args(["files_from"]).requires("unstable")),
+    group(ArgGroup::new("unstable-files-from-stdin").args(["files_from_stdin"]).requires("unstable")),
     group(ArgGroup::new("unstable-delete-exclude").args(["exclude"]).requires("unstable")),
     group(ArgGroup::new("unstable-exclude-from").args(["exclude_from"]).requires("unstable")),
+    group(ArgGroup::new("read-files-from").args(["files_from", "files_from_stdin"])),
 )]
 pub(crate) struct DeleteCommand {
     #[arg(long, help = "Output file path", value_hint = ValueHint::FilePath)]
     output: Option<PathBuf>,
+    #[arg(long, help = "Read deleting files from given path (unstable)", value_hint = ValueHint::FilePath)]
+    files_from: Option<String>,
+    #[arg(long, help = "Read deleting files from stdin (unstable)")]
+    files_from_stdin: bool,
     #[arg(long, help = "Exclude path glob (unstable)", value_hint = ValueHint::AnyPath)]
     exclude: Option<Vec<String>>,
     #[arg(long, help = "Read exclude files from given path (unstable)", value_hint = ValueHint::FilePath)]
@@ -41,8 +48,14 @@ impl Command for DeleteCommand {
 
 fn delete_file_from_archive(args: DeleteCommand) -> io::Result<()> {
     let password = ask_password(args.password)?;
-    let globs = GlobPatterns::new(args.file.files)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    let mut files = args.file.files;
+    if args.files_from_stdin {
+        files.extend(io::stdin().lines().collect::<io::Result<Vec<_>>>()?);
+    } else if let Some(path) = args.files_from {
+        files.extend(utils::fs::read_to_lines(path)?);
+    }
+    let globs =
+        GlobPatterns::new(files).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
     let exclude_globs = {
         let mut exclude = args.exclude.unwrap_or_default();
         if let Some(p) = args.exclude_from {
