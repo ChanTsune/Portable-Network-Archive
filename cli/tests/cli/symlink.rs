02fbfd42db3d2c330e5c7d15b79e8935a227f6a7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-10 09:32:21 +0900

    :white_check_mark: More strictly check in test for symlink

--- cli/tests/cli/symlink.rs
+++ cli/tests/cli/symlink.rs
@@ -1,9 +1,11 @@
 use crate::utils::setup;
 use clap::Parser;
 use portable_network_archive::{cli, command};
-use std::io::Write;
-use std::path::Path;
-use std::{fs, os};
+use std::{
+    fs,
+    io::prelude::*,
+    path::{Path, PathBuf},
+};
 
 fn init_resource<P: AsRef<Path>>(dir: P) {
     let dir = dir.as_ref();
@@ -13,35 +15,34 @@ fn init_resource<P: AsRef<Path>>(dir: P) {
     fs::create_dir_all(dir).unwrap();
     let mut file = fs::File::create(dir.join("text.txt")).unwrap();
     file.write_all(b"content").unwrap();
-    #[cfg(unix)]
-    os::unix::fs::symlink(Path::new("text.txt"), dir.join("link.txt")).unwrap();
-    #[cfg(windows)]
-    os::windows::fs::symlink_file(Path::new("text.txt"), dir.join("link.txt")).unwrap();
-    #[cfg(target_os = "wasi")]
-    os::wasi::fs::symlink_path(Path::new("text.txt"), dir.join("link.txt")).unwrap();
+    pna::fs::symlink(Path::new("text.txt"), dir.join("link.txt")).unwrap();
 }
 
 #[test]
-fn symlink() {
+fn symlink_no_follow() {
     setup();
-    init_resource(format!("{}/symlink/source", env!("CARGO_TARGET_TMPDIR")));
+    init_resource("symlink_no_follow/source");
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "c",
-        &format!("{}/symlink.pna", env!("CARGO_TARGET_TMPDIR")),
+        "symlink_no_follow/symlink_no_follow.pna",
         "--overwrite",
-        &format!("{}/symlink/source", env!("CARGO_TARGET_TMPDIR")),
+        "symlink_no_follow/source",
     ]))
     .unwrap();
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
         "x",
-        &format!("{}/symlink.pna", env!("CARGO_TARGET_TMPDIR")),
+        "symlink_no_follow/symlink_no_follow.pna",
         "--overwrite",
         "--out-dir",
-        &format!("{}/symlink/dist", env!("CARGO_TARGET_TMPDIR")),
+        "symlink_no_follow/dist",
+        "--strip-components",
+        "2",
     ]))
     .unwrap();
+
+    assert!(PathBuf::from("symlink_no_follow/dist/link.txt").is_symlink());
 }
