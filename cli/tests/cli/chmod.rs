f469a1f88d736303e062d8a99d09dede4950dc07
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-02-11 22:47:27 +0900

    :white_check_mark: more strictly check `chmod` subcommand

--- cli/tests/cli/chmod.rs
+++ cli/tests/cli/chmod.rs
@@ -1,11 +1,19 @@
 use crate::utils::{diff::diff, setup, TestResources};
 use clap::Parser;
 use portable_network_archive::{cli, command};
+#[cfg(unix)]
+use std::fs;
+#[cfg(unix)]
+use std::os::unix::prelude::*;
 
 #[test]
 fn archive_chmod() {
     setup();
     TestResources::extract_in("raw/", "chmod/in/").unwrap();
+
+    #[cfg(unix)]
+    fs::set_permissions("chmod/in/raw/text.txt", fs::Permissions::from_mode(0o777)).unwrap();
+
     command::entry(cli::Cli::parse_from([
         "pna",
         "--quiet",
@@ -25,17 +33,7 @@ fn archive_chmod() {
         "chmod",
         "chmod/chmod.pna",
         "--",
-        "-w",
-        "chmod/in/raw/text.txt",
-    ]))
-    .unwrap();
-    command::entry(cli::Cli::parse_from([
-        "pna",
-        "--quiet",
-        "experimental",
-        "chmod",
-        "chmod/chmod.pna",
-        "+w",
+        "-x",
         "chmod/in/raw/text.txt",
     ]))
     .unwrap();
@@ -54,6 +52,11 @@ fn archive_chmod() {
         "2",
     ]))
     .unwrap();
+    #[cfg(unix)]
+    {
+        let meta = fs::symlink_metadata("chmod/out/raw/text.txt").unwrap();
+        assert_eq!(meta.permissions().mode() & 0o777, 0o666);
+    }
 
     diff("chmod/in/", "chmod/out/").unwrap();
 }
