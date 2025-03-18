04665184ed5e3e47b361b51c5882f7611f792ab8
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-03-18 14:09:06 +0900

    :memo: Added `# Errors` section to documentation comment of `libpna::read_as_chunks` and `libpna::read_chunks_from_slice`

--- lib/src/chunk.rs
+++ lib/src/chunk.rs
@@ -270,7 +270,12 @@ pub(crate) fn chunk_data_split(
     }
 }
 
-/// Read archive as chunks
+/// Read archive as chunks from given reader.
+///
+/// Reads a PNA archive from the given reader and returns an iterator of chunks.
+///
+/// # Errors
+/// Returns error if it is not PNA file.
 ///
 /// # Example
 ///
@@ -318,7 +323,12 @@ pub fn read_as_chunks<R: Read>(
     })
 }
 
-/// Read chunks from archive slice
+/// Read archive as chunks from given bytes.
+///
+/// Reads a PNA archive from the given byte slice and returns an iterator of chunks.
+///
+/// # Errors
+/// Returns error if it is not PNA file.
 ///
 /// # Example
 ///
