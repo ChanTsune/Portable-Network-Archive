2f9805067f9bd7f48732d5f4d66681474d0614ce
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-05-11 22:56:09 +0900

    :pencil2: Fixed missing article

--- lib/src/archive/read.rs
+++ lib/src/archive/read.rs
@@ -436,7 +436,7 @@ impl<R: Read + Seek> Archive<R> {
     /// Seek the cursor to the end of the archive marker.
     ///
     /// # Errors
-    /// Returns an error if this function failed to seek or contains broken chunk.
+    /// Returns an error if this function failed to seek or contains a broken chunk.
     ///
     /// # Examples
     /// For appending entry to the existing archive.
