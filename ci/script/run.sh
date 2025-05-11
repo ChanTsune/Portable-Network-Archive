7113fbff830c3a468054e7f6076368f5d7c30a82
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-05-11 21:12:20 +0900

    :construction_worker: :recycle: use trap command for exec teardown

--- ci/script/run.sh
+++ ci/script/run.sh
@@ -17,17 +17,17 @@ run_with() {
   PNA_FILE="$1"
   OPTIONS=${*:2}
   PNA_FILE_PATH="${RESOURCE_DIR}${PNA_FILE}"
+  trap 'rm -rf "${PNA_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"' RETURN
   pna create ${OPTIONS} --overwrite "${PNA_FILE_PATH}" "${TEST_FILE_PATH}"
   pna extract --overwrite --out-dir "${RESOURCE_DIR}" "${PNA_FILE_PATH}"
   cmp "${TEST_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"
-  rm -rf "${PNA_FILE_PATH}" "${RESOURCE_DIR}${TEST_FILE_PATH}"
 }
 
 main() {
   setup
+  trap teardown RETURN
   run_with "store.pna" --store
   run_with "store_solid.pna" --store --solid
-  teardown
 }
 
 main "$@"
