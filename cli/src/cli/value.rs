52502215e243b1ddff7a18240d959b1d6511213f
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-05-24 22:17:24 +0900

    :truck: Move Pbkdf2Sha256Params to cli::value::pbkdf2_sha256_params
    Moved the PBKDF2-SHA256 CLI parameter parser out of `cli.rs` into its
    own dedicated module to improve maintainability.

--- cli/src/cli/value.rs
+++ cli/src/cli/value.rs
@@ -1,5 +1,7 @@
 pub(crate) mod argon2id_params;
+pub(crate) mod pbkdf2_sha256_params;
 pub(crate) mod private_chunk_type;
 
 pub(crate) use argon2id_params::Argon2idParams;
+pub(crate) use pbkdf2_sha256_params::Pbkdf2Sha256Params;
 pub(crate) use private_chunk_type::PrivateChunkType;
