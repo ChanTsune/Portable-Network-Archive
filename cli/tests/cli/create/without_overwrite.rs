734e284083997fe014d8ab0e9cf34ef5d264c722
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-05-29 16:18:14 +0900

    :white_check_mark: Added test case for fail when not using overwrite

---
+++ cli/tests/cli/create/without_overwrite.rs
@@ -0,0 +1,28 @@
+use crate::utils::{setup, TestResources};
+use clap::Parser;
+use portable_network_archive::{cli, command::Command};
+use std::{fs, io::ErrorKind};
+
+#[test]
+fn fail_without_overwrite() {
+    setup();
+    TestResources::extract_in("raw/", "create_without_overwrite/src/").unwrap();
+    let archive = "create_without_overwrite/create_without_overwrite.pna";
+    fs::create_dir_all("create_without_overwrite").unwrap();
+    fs::write(archive, b"exist").unwrap();
+
+    let result = cli::Cli::try_parse_from([
+        "pna",
+        "--quiet",
+        "c",
+        archive,
+        "create_without_overwrite/src/",
+    ])
+    .unwrap()
+    .execute();
+
+    assert!(matches!(
+        result.unwrap_err().kind(),
+        ErrorKind::AlreadyExists
+    ));
+}
