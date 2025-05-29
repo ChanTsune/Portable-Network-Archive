b0695d1c8579db61ad37e386801516444c20c6ec
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-05-29 16:10:15 +0900

    :recycle: Use criterion for benchmarks

--- cli/benches/list.rs
+++ cli/benches/list.rs
@@ -1,33 +1,35 @@
-#![feature(test)]
-extern crate test;
-
 use clap::Parser;
+use criterion::{criterion_group, criterion_main, Criterion};
 use portable_network_archive::{cli, command};
-use test::Bencher;
 
-#[bench]
-fn normal(b: &mut Bencher) {
-    b.iter(|| {
-        command::entry(cli::Cli::parse_from([
-            "pna",
-            "--quiet",
-            "ls",
-            "../resources/test/zstd.pna",
-        ]))
-        .unwrap()
-    })
+fn bench_normal(c: &mut Criterion) {
+    c.bench_function("normal", |b| {
+        b.iter(|| {
+            command::entry(cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "../resources/test/zstd.pna",
+            ]))
+            .unwrap()
+        })
+    });
 }
 
-#[bench]
-fn solid(b: &mut Bencher) {
-    b.iter(|| {
-        command::entry(cli::Cli::parse_from([
-            "pna",
-            "--quiet",
-            "ls",
-            "--solid",
-            "../resources/test/solid_zstd.pna",
-        ]))
-        .unwrap()
-    })
+fn bench_solid(c: &mut Criterion) {
+    c.bench_function("solid", |b| {
+        b.iter(|| {
+            command::entry(cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "--solid",
+                "../resources/test/solid_zstd.pna",
+            ]))
+            .unwrap()
+        })
+    });
 }
+
+criterion_group!(benches, bench_normal, bench_solid);
+criterion_main!(benches);
--- cli/benches/split.rs
+++ cli/benches/split.rs
@@ -1,61 +1,69 @@
-#![feature(test)]
-extern crate test;
-
 use clap::Parser;
+use criterion::{criterion_group, criterion_main, Criterion};
 use portable_network_archive::{cli, command};
-use test::Bencher;
 
-#[bench]
-fn create_with_split(b: &mut Bencher) {
-    b.iter(|| {
-        command::entry(cli::Cli::parse_from([
-            "pna",
-            "--quiet",
-            "c",
-            &format!(
-                "{}/bench/create_with_split/store.pna",
-                env!("CARGO_TARGET_TMPDIR")
-            ),
-            "--store",
-            "--split",
-            "3MB",
-            "--overwrite",
-            "../resources/test/raw/",
-        ]))
-        .unwrap()
-    })
+fn bench_create_with_split(c: &mut Criterion) {
+    c.bench_function("create_with_split", |b| {
+        b.iter(|| {
+            command::entry(cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "c",
+                &format!(
+                    "{}/bench/create_with_split/store.pna",
+                    env!("CARGO_TARGET_TMPDIR")
+                ),
+                "--store",
+                "--split",
+                "3MB",
+                "--overwrite",
+                "../resources/test/raw/",
+            ]))
+            .unwrap()
+        })
+    });
 }
 
-#[bench]
-fn split(b: &mut Bencher) {
-    b.iter(|| {
-        command::entry(cli::Cli::parse_from([
-            "pna",
-            "--quiet",
-            "split",
-            "../resources/test/store.pna",
-            "--overwrite",
-            "--max-size",
-            "3MB",
-            "--out-dir",
-            &format!("{}/bench/split/", env!("CARGO_TARGET_TMPDIR")),
-        ]))
-        .unwrap()
-    })
+fn bench_split(c: &mut Criterion) {
+    c.bench_function("split", |b| {
+        b.iter(|| {
+            command::entry(cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "split",
+                "../resources/test/store.pna",
+                "--overwrite",
+                "--max-size",
+                "3MB",
+                "--out-dir",
+                &format!("{}/bench/split/", env!("CARGO_TARGET_TMPDIR")),
+            ]))
+            .unwrap()
+        })
+    });
 }
 
-#[bench]
-fn extract_multipart(b: &mut Bencher) {
-    b.iter(|| {
-        command::entry(cli::Cli::parse_from([
-            "pna",
-            "--quiet",
-            "x",
-            "../resources/test/multipart.part1.pna",
-            "--overwrite",
-            "--out-dir",
-            &format!("{}/bench/multipart/", env!("CARGO_TARGET_TMPDIR")),
-        ]))
-        .unwrap()
-    })
+fn bench_extract_multipart(c: &mut Criterion) {
+    c.bench_function("extract_multipart", |b| {
+        b.iter(|| {
+            command::entry(cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "x",
+                "../resources/test/multipart.part1.pna",
+                "--overwrite",
+                "--out-dir",
+                &format!("{}/bench/multipart/", env!("CARGO_TARGET_TMPDIR")),
+            ]))
+            .unwrap()
+        })
+    });
 }
+
+criterion_group!(
+    benches,
+    bench_create_with_split,
+    bench_split,
+    bench_extract_multipart
+);
+criterion_main!(benches);
