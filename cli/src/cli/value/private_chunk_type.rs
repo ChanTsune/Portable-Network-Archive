b5e28c17156b400536752d74b7316b5a7bc7183b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-06-08 15:27:47 +0900

    :white_check_mark: Expand test cases for `PrivateChunkType`

--- cli/src/cli/value/private_chunk_type.rs
+++ cli/src/cli/value/private_chunk_type.rs
@@ -16,3 +16,46 @@ impl FromStr for PrivateChunkType {
         )?))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str_valid() {
+        let chunk_type = PrivateChunkType::from_str("myTy").unwrap();
+        assert_eq!(chunk_type.0, ChunkType::private(*b"myTy").unwrap());
+    }
+
+    #[test]
+    fn from_str_invalid_length() {
+        assert_eq!(
+            PrivateChunkType::from_str("invalid").unwrap_err(),
+            ChunkTypeError::NonPrivateChunkType
+        );
+    }
+
+    #[test]
+    fn from_str_invalid_second_char() {
+        assert_eq!(
+            PrivateChunkType::from_str("pRIv").unwrap_err(),
+            ChunkTypeError::NonPrivateChunkType
+        );
+    }
+
+    #[test]
+    fn from_str_invalid_third_char() {
+        assert_eq!(
+            PrivateChunkType::from_str("rese").unwrap_err(),
+            ChunkTypeError::Reserved
+        );
+    }
+
+    #[test]
+    fn from_str_non_ascii() {
+        assert_eq!(
+            PrivateChunkType::from_str("zeR\0").unwrap_err(),
+            ChunkTypeError::NonAsciiAlphabetic
+        );
+    }
+}
