54c20210b5aa728b7e2c85e0faff4ae5bb70ec01
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-06-14 14:25:43 +0900

    :white_check_mark: Added list benchmark cases

--- cli/benches/list.rs
+++ cli/benches/list.rs
@@ -9,7 +9,109 @@ fn bench_list_normal(c: &mut Criterion) {
                 "pna",
                 "--quiet",
                 "ls",
-                concat!(env!("CARGO_MANIFEST_DIR"), "/../resources/test/zstd.pna"),
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
+            ])
+            .execute()
+            .unwrap()
+        })
+    });
+}
+
+fn bench_list_normal_classify(c: &mut Criterion) {
+    c.bench_function("list_normal_classify", |b| {
+        b.iter(|| {
+            cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "--classify",
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
+            ])
+            .execute()
+            .unwrap()
+        })
+    });
+}
+
+fn bench_list_normal_hide_control_chars(c: &mut Criterion) {
+    c.bench_function("list_normal_hide_control_chars", |b| {
+        b.iter(|| {
+            cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "-q",
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
+            ])
+            .execute()
+            .unwrap()
+        })
+    });
+}
+
+fn bench_list_normal_table(c: &mut Criterion) {
+    c.bench_function("list_normal_table", |b| {
+        b.iter(|| {
+            cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "-l",
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
+            ])
+            .execute()
+            .unwrap()
+        })
+    });
+}
+
+fn bench_list_normal_jsonl(c: &mut Criterion) {
+    c.bench_function("list_normal_jsonl", |b| {
+        b.iter(|| {
+            cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "--format",
+                "jsonl",
+                "--unstable",
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
+            ])
+            .execute()
+            .unwrap()
+        })
+    });
+}
+
+fn bench_list_normal_tree(c: &mut Criterion) {
+    c.bench_function("list_normal_tree", |b| {
+        b.iter(|| {
+            cli::Cli::parse_from([
+                "pna",
+                "--quiet",
+                "ls",
+                "--format",
+                "tree",
+                "--unstable",
+                concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../resources/test/zstd_keep_dir.pna"
+                ),
             ])
             .execute()
             .unwrap()
@@ -36,5 +138,14 @@ fn bench_list_solid(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_list_normal, bench_list_solid);
+criterion_group!(
+    benches,
+    bench_list_normal,
+    bench_list_normal_classify,
+    bench_list_normal_hide_control_chars,
+    bench_list_normal_table,
+    bench_list_normal_jsonl,
+    bench_list_normal_tree,
+    bench_list_solid,
+);
 criterion_main!(benches);
