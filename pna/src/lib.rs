a0c2816c19d46701e7e21c7a7d78ba2b28bf6f85
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-08-21 10:25:16 +0900

    :pencil2: Improve and clarify `pna` documentation comment
    Refines and standardizes documentation comments across archive, entry, metadata, and filesystem modules. Updates phrasing for clarity, consistency, and accuracy, and improves error descriptions and trait explanations. No functional code changes were made.

--- pna/src/lib.rs
+++ pna/src/lib.rs
@@ -1,7 +1,7 @@
 //! A library for more useful reading and writing PNA archives
 //!
-//! This library provides filesystem-related utilities in addition to utilities
-//! necessary to manage PNA archives abstracted over a reader or writer hosted by [libpna].
+//! Provides filesystem-related utilities in addition to the utilities
+//! necessary to manage PNA archives abstracted over a reader or writer hosted by [`libpna`].
 #![cfg_attr(target_os = "wasi", feature(wasi_ext))]
 #![doc(html_root_url = "https://docs.rs/pna/0.27.0")]
 #![deny(
