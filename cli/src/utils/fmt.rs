0ccd912eacdd8a955f1c0be4a3194971c0ac84db
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-09-13 16:50:10 +0900

    :memo: Add doc comment to DurationDisplay struct
    Added a documentation comment to the DurationDisplay struct to clarify its purpose as a human-friendly duration display.

--- cli/src/utils/fmt.rs
+++ cli/src/utils/fmt.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use std::time::Duration;
 
 #[derive(Debug)]
+/// Displays a human‑friendly duration.
 pub(crate) struct DurationDisplay(pub(crate) Duration);
 
 impl fmt::Display for DurationDisplay {
