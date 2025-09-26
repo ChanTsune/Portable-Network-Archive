5e052b750f00b1ddb5e9bafc40cbaf9288a647fe
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-09-26 12:08:44 +0900

    :boom: Change --password-file behavior
    Previously, the entire file content was used as the password.  Now, only the first non-empty line is read, and trailing newlines are ignored,  to align with common tool behavior.

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -20,12 +20,21 @@ pub(crate) mod strip;
 pub mod update;
 pub mod xattr;
 
-use crate::cli::{CipherAlgorithmArgs, Cli, Commands, PasswordArgs};
-use std::{fs, io};
+use crate::{
+    cli::{CipherAlgorithmArgs, Cli, Commands, PasswordArgs},
+    utils::fs,
+};
+use std::io;
 
 fn ask_password(args: PasswordArgs) -> io::Result<Option<String>> {
     if let Some(path) = args.password_file {
-        return Ok(Some(fs::read_to_string(path)?));
+        return match fs::read_first_non_empty_line(path)? {
+            password @ Some(_) => Ok(password),
+            None => Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "password is empty",
+            )),
+        };
     };
     Ok(match args.password {
         Some(password @ Some(_)) => {
