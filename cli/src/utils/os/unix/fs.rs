5a79a8c927e4aae4adbbaf7d66120975280f8121
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-10-08 07:12:01 +0900

    :bug: Fix failed to restore permissions on broken symlink
    Replaces direct use of fs::set_permissions with a new utils::os::unix::fs::chmod function. This change improves handling of broken symlinks by safely ignoring permission changes on them, preventing errors when setting permissions on non-existent symlink targets.

--- cli/src/utils/os/unix/fs.rs
+++ cli/src/utils/os/unix/fs.rs
@@ -1,5 +1,21 @@
 #[cfg(not(target_os = "redox"))]
 pub(crate) mod owner;
+
 #[cfg(target_os = "redox")]
 pub(crate) use crate::utils::os::redox::fs::owner;
+use std::{fs, io, os::unix::fs::PermissionsExt, path::Path};
 pub(crate) mod xattrs;
+
+#[inline]
+pub(crate) fn chmod(path: &Path, mode: u16) -> io::Result<()> {
+    match fs::set_permissions(path, fs::Permissions::from_mode(mode.into())) {
+        Err(e)
+            if e.kind() == io::ErrorKind::NotFound
+                && fs::symlink_metadata(path).is_ok_and(|m| m.file_type().is_symlink()) =>
+        {
+            // NOTE: broken symlink will never success set permissions
+            Ok(())
+        }
+        result => result,
+    }
+}
