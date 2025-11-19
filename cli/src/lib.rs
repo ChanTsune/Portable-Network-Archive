6403ad602a7989fec636fa8facdac0846ba3b441
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-11-19 23:07:37 +0900

    :bookmark: Bump portable-network-archive version to 0.28.4

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/portable-network-archive/0.28.3")]
+#![doc(html_root_url = "https://docs.rs/portable-network-archive/0.28.4")]
 #![doc = include_str!("../README.md")]
 mod chunk;
 pub mod cli;
