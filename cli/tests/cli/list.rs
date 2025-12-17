4ea8b6487da58643b40b51b091e117dd4aaee64a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-12-17 15:05:08 +0900

    bsdtar format

--- cli/tests/cli/list.rs
+++ cli/tests/cli/list.rs
@@ -2,6 +2,8 @@
 mod exclude_vcs;
 mod missing_file;
 #[cfg(not(target_family = "wasm"))]
+mod option_format_bsdtar;
+#[cfg(not(target_family = "wasm"))]
 mod option_format_jsonl;
 #[cfg(not(target_family = "wasm"))]
 mod option_format_line;
