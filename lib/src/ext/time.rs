b027bb18f8a554f77d38add7c71141b939db90ce
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-04-02 21:10:51 +0900

    :memo: Add Examples section to SystemTimeExt::duration_since_unix_epoch_signed

--- lib/src/ext/time.rs
+++ lib/src/ext/time.rs
@@ -3,12 +3,27 @@ use std::time::SystemTime;
 
 /// [`SystemTime`] extension trait.
 pub trait SystemTimeExt {
-    /// Returns the duration since the Unix epoch.
+    /// Returns the duration since the Unix epoch as a signed [`Duration`].
+    ///
+    /// The result is positive for times after the epoch and negative for times
+    /// before it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::time::{Duration, SystemTime};
+    /// use libpna::prelude::SystemTimeExt;
+    ///
+    /// let epoch = SystemTime::UNIX_EPOCH;
+    /// assert!(epoch.duration_since_unix_epoch_signed().is_zero());
+    ///
+    /// let after = SystemTime::UNIX_EPOCH + Duration::from_secs(100);
+    /// assert!(after.duration_since_unix_epoch_signed().is_positive());
+    /// ```
     fn duration_since_unix_epoch_signed(&self) -> Duration;
 }
 
 impl SystemTimeExt for SystemTime {
-    /// Returns the duration since the Unix epoch.
     #[inline]
     fn duration_since_unix_epoch_signed(&self) -> Duration {
         time::OffsetDateTime::from(*self) - time::OffsetDateTime::UNIX_EPOCH
