f4cc994b9bf128513c89b09440bf44fbca358ee5
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-08-23 11:38:42 +0900

    :boom: Hide the deprecated permission setters

--- cli/src/command/chown.rs
+++ cli/src/command/chown.rs
@@ -98,7 +98,6 @@ impl EntryTransform for ChownTransform<'_> {
 }
 
 #[inline]
-#[allow(deprecated)]
 fn transform_entry<T>(entry: NormalEntry<T>, owner: &Ownership) -> NormalEntry<T> {
     let metadata = entry.metadata().clone();
     let own = crate::ext::ResolvedOwnership::from_metadata(&metadata);
@@ -122,7 +121,6 @@ fn transform_entry<T>(entry: NormalEntry<T>, owner: &Ownership) -> NormalEntry<T
     };
     let metadata =
         metadata
-            .with_permission(None)
             .with_owner_uid(uid.map(pna::OwnerUid::from))
             .with_owner_gid(gid.map(pna::OwnerGid::from))
             .with_owner_user_name(crate::command::core::permission::owner_name_opt(&uname))
